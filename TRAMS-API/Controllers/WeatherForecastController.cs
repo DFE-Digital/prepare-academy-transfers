85b7cfdf095d8cb1cd5871079b5f812bfa826485
Mihail Andrici <Mihail.ANDRICI@EDUCATION.GOV.UK>
2020-11-27 15:47:35 +0000

    Adds connectivity test on default controller

--- TRAMS-API/Controllers/WeatherForecastController.cs
+++ TRAMS-API/Controllers/WeatherForecastController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
 
 namespace TRAMS_API.Controllers
 {
@@ -17,23 +21,35 @@ namespace TRAMS_API.Controllers
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IConfiguration _config;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<string> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            var authority = _config["D365:Authority"];
+            var clientId = _config["D365:ClientId"];
+            var clientSecret = _config["D365:ClientSecret"];
+            var url = _config["D365:Url"];
+            
+            var authContext = new AuthenticationContext(authority);
+            var clientCredential = new ClientCredential(clientId, clientSecret);
+            var token = await authContext.AcquireTokenAsync(url, clientCredential);
+
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token.AccessToken);
+
+            var result = await client.GetAsync($"{url}/api/data/v9.1/accounts?$select=name&$filter=statecode eq 0");
+
+            var casterd = await result.Content.ReadAsStringAsync();
+
+            return $"Request Status Code: {result.StatusCode}. First 200 chars of response are: {casterd.Substring(0, 200)}";
         }
     }
 }
