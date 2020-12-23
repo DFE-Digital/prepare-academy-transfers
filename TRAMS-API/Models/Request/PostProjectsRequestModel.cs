8359507f885413072eaa1ce96d21abc38b2dd8fc
Mihail Andrici <Mihail.ANDRICI@EDUCATION.GOV.UK>
2020-12-23 12:31:00 +0000

    Add Fluent Validation

---
+++ TRAMS-API/Models/Request/PostProjectsRequestModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models.Request
+{
+    public class PostProjectsRequestModel
+    {
+        [Required]
+        public string ProjectInitiatorFullName { get; set; }
+
+        [Required]
+        public string ProjectInitiatorUid { get; set; }
+
+        [Required]
+        public int ProjectStatus { get; set; }
+
+        public List<PostProjectsAcademiesModel> ProjectAcademies { get; set;}
+
+        public List<PostProjectsTrustsModel> ProjectTrusts { get; set; }
+    }
+
+    public class PostProjectsAcademiesModel
+    {
+        [Required]
+        public Guid AcademyId { get; set; }
+
+        public List<int> EsfaInterventionReasons { get; set; }
+
+        public string EsfaInterventionReasonsExplained { get; set; }
+
+        public List<int> RddOrRscInterventionReasons { get; set; }
+
+        public string RddOrRscInterventionReasonsExplained { get; set; }
+
+        public List<PostProjectsAcademiesTrustsModel> Trusts { get; set; }
+    }
+
+    public class PostProjectsAcademiesTrustsModel
+    {
+        [Required]
+        public Guid TrustId { get; set; }
+    }
+
+    public class PostProjectsTrustsModel
+    {
+        [Required]
+        public Guid TrustId { get; set; }
+    }
+}
