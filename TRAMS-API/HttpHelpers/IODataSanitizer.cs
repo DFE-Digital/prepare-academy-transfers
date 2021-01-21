9f0e479c655ae4a1ca67858a62a888868cb9699b
Mihail Andrici <Mihail.ANDRICI@EDUCATION.GOV.UK>
2021-01-21 15:40:59 +0000

    Sanitize OData search query

---
+++ TRAMS-API/HttpHelpers/IODataSanitizer.cs
@@ -0,0 +1,12 @@
+﻿namespace API.HttpHelpers
+{
+    public interface IODataSanitizer
+    {
+        /// <summary>
+        /// Sanitizes search strings for use in OData filters
+        /// </summary>
+        /// <param name="input">The input search string</param>
+        /// <returns></returns>
+        public string Sanitize(string input);
+    }
+}
