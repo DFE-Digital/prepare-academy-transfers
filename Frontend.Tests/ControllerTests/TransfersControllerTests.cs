5fd71667f1310caf61b57181a7264179884938b5
Daniel Burnley <dan@danops.uk>
2021-02-17 14:22:06 +0000

    Add confirm outgoing trusts route

--- Frontend.Tests/ControllerTests/TransfersControllerTests.cs
+++ Frontend.Tests/ControllerTests/TransfersControllerTests.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using API.Mapping;
 using API.Models.Downstream.D365;
 using API.Models.Upstream.Response;
@@ -8,8 +11,10 @@ using API.Repositories;
 using Frontend.Controllers;
 using Frontend.Views.Transfers;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Session;
 using Moq;
 using Xunit;
 
@@ -17,23 +22,85 @@ namespace Frontend.Tests.ControllerTests
 {
     public class TransfersControllerTests
     {
+        class MockSession : ISession
+        {
+            public readonly Dictionary<string, string> SessionStore;
+
+            public MockSession()
+            {
+                SessionStore = new Dictionary<string, string>();
+            }
+
+            public void Clear()
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task CommitAsync(CancellationToken cancellationToken = new CancellationToken())
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task LoadAsync(CancellationToken cancellationToken = new CancellationToken())
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Remove(string key)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Set(string key, byte[] value)
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool TryGetValue(string key, out byte[] value)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void SetString(string key, string value)
+            {
+                SessionStore[key] = value;
+            }
+
+            public string Id { get; }
+            public bool IsAvailable { get; }
+            public IEnumerable<string> Keys { get; }
+        }
+
         private readonly Mock<ITrustsRepository> _trustRepository;
+        private readonly Mock<IAcademiesRepository> _academiesRepository;
         private readonly Mock<IMapper<GetTrustsD365Model, GetTrustsModel>> _getTrustMapper;
+        private readonly Mock<IMapper<GetAcademiesD365Model, GetAcademiesModel>> _getAcademiesMapper;
         private readonly TransfersController _subject;
+        private readonly Mock<ISession> _session;
 
         public TransfersControllerTests()
         {
             _trustRepository = new Mock<ITrustsRepository>();
+            _academiesRepository = new Mock<IAcademiesRepository>();
             _getTrustMapper = new Mock<IMapper<GetTrustsD365Model, GetTrustsModel>>();
+            _getAcademiesMapper = new Mock<IMapper<GetAcademiesD365Model, GetAcademiesModel>>();
+            _session = new Mock<ISession>();
 
             var tempDataProvider = new Mock<ITempDataProvider>();
+            var httpContext = new DefaultHttpContext();
+            var sessionFeature = new SessionFeature {Session = _session.Object};
+            httpContext.Features.Set<ISessionFeature>(sessionFeature);
+
             var tempDataDictionaryFactory =
                 new TempDataDictionaryFactory(tempDataProvider.Object);
-            var tempData = tempDataDictionaryFactory.GetTempData(new DefaultHttpContext());
+            var tempData = tempDataDictionaryFactory.GetTempData(httpContext);
 
             _subject = new TransfersController(
                 _trustRepository.Object,
-                _getTrustMapper.Object) {TempData = tempData};
+                _academiesRepository.Object,
+                _getTrustMapper.Object,
+                _getAcademiesMapper.Object
+            ) {TempData = tempData, ControllerContext = {HttpContext = httpContext}};
         }
 
         #region TrustName
@@ -106,32 +173,6 @@ namespace Frontend.Tests.ControllerTests
             AssertTrustsAreMappedCorrectly(trustId, trustTwoId);
         }
 
-        private void AssertTrustsAreMappedCorrectly(Guid trustId, Guid trustTwoId)
-        {
-            _getTrustMapper.Verify(m => m.Map(It.Is<GetTrustsD365Model>(model => model.Id == trustId)), Times.Once);
-            _getTrustMapper.Verify(m => m.Map(It.Is<GetTrustsD365Model>(model => model.Id == trustTwoId)), Times.Once);
-        }
-
-        private void AssertTrustRepositoryIsCalledCorrectly()
-        {
-            _trustRepository.Verify(r => r.SearchTrusts("Trust name"));
-        }
-
-        private static void AssertTrustsAreAssignedToTheView(IActionResult result, Guid trustId, Guid trustTwoId)
-        {
-            var viewResult = Assert.IsType<ViewResult>(result);
-            var viewModel = Assert.IsType<TrustSearch>(viewResult.ViewData.Model);
-
-            Assert.Equal(trustId, viewModel.Trusts[0].Id);
-            Assert.Equal(trustTwoId, viewModel.Trusts[1].Id);
-        }
-
-        private static void AssertRedirectToTrustName(IActionResult response)
-        {
-            var redirectResponse = Assert.IsType<RedirectToActionResult>(response);
-            Assert.Equal("TrustName", redirectResponse.ActionName);
-        }
-
         #endregion
 
         #region OutgoingTrustDetails
@@ -175,6 +216,53 @@ namespace Frontend.Tests.ControllerTests
             var viewModel = Assert.IsType<OutgoingTrustDetails>(viewResponse.Model);
 
             Assert.Equal(mappedTrust, viewModel.Trust);
+        }
+
+        #endregion
+
+        #region ConfirmOutgoingTrust
+
+        [Fact]
+        public void GivenTrustGuid_StoresTheTrustInTheSessionAndRedirects()
+        {
+            var trustId = Guid.Parse("9a7be920-eaa0-e911-a83f-000d3a3852af");
+            _subject.ConfirmOutgoingTrust(trustId);
+
+            _session.Verify(s => s.Set(
+                "OutgoingTrustId",
+                It.Is<byte[]>(input =>
+                    Encoding.UTF8.GetString(input) == trustId.ToString()
+                )));
+        }
+
+        #endregion
+
+        #region HelperMethods
+
+        private void AssertTrustsAreMappedCorrectly(Guid trustId, Guid trustTwoId)
+        {
+            _getTrustMapper.Verify(m => m.Map(It.Is<GetTrustsD365Model>(model => model.Id == trustId)), Times.Once);
+            _getTrustMapper.Verify(m => m.Map(It.Is<GetTrustsD365Model>(model => model.Id == trustTwoId)), Times.Once);
+        }
+
+        private void AssertTrustRepositoryIsCalledCorrectly()
+        {
+            _trustRepository.Verify(r => r.SearchTrusts("Trust name"));
+        }
+
+        private static void AssertTrustsAreAssignedToTheView(IActionResult result, Guid trustId, Guid trustTwoId)
+        {
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var viewModel = Assert.IsType<TrustSearch>(viewResult.ViewData.Model);
+
+            Assert.Equal(trustId, viewModel.Trusts[0].Id);
+            Assert.Equal(trustTwoId, viewModel.Trusts[1].Id);
+        }
+
+        private static void AssertRedirectToTrustName(IActionResult response)
+        {
+            var redirectResponse = Assert.IsType<RedirectToActionResult>(response);
+            Assert.Equal("TrustName", redirectResponse.ActionName);
         }
 
         #endregion
--- Frontend/Controllers/TransfersController.cs
+++ Frontend/Controllers/TransfersController.cs
@@ -6,6 +6,7 @@ using API.Models.Downstream.D365;
 using API.Models.Upstream.Response;
 using API.Repositories;
 using Frontend.Views.Transfers;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Frontend.Controllers
@@ -13,13 +14,18 @@ namespace Frontend.Controllers
     public class TransfersController : Controller
     {
         private readonly ITrustsRepository _trustRepository;
+        private readonly IAcademiesRepository _academiesRepository;
         private readonly IMapper<GetTrustsD365Model, GetTrustsModel> _getTrustMapper;
+        private readonly IMapper<GetAcademiesD365Model, GetAcademiesModel> _getAcademiesMapper;
 
-        public TransfersController(ITrustsRepository trustRepository,
-            IMapper<GetTrustsD365Model, GetTrustsModel> getTrustMapper)
+        public TransfersController(ITrustsRepository trustRepository, IAcademiesRepository academiesRepository,
+            IMapper<GetTrustsD365Model, GetTrustsModel> getTrustMapper,
+            IMapper<GetAcademiesD365Model, GetAcademiesModel> getAcademiesMapper)
         {
             _trustRepository = trustRepository;
+            _academiesRepository = academiesRepository;
             _getTrustMapper = getTrustMapper;
+            _getAcademiesMapper = getAcademiesMapper;
         }
 
         public IActionResult TrustName()
@@ -60,6 +66,23 @@ namespace Frontend.Controllers
             var result = await _trustRepository.GetTrustById(trustId);
             var mappedResult = _getTrustMapper.Map(result.Result);
             var model = new OutgoingTrustDetails {Trust = mappedResult};
+            return View(model);
+        }
+
+        public IActionResult ConfirmOutgoingTrust(Guid trustId)
+        {
+            HttpContext.Session.SetString("OutgoingTrustId", trustId.ToString());
+
+            return RedirectToAction("OutgoingTrustAcademies");
+        }
+
+        public async Task<IActionResult> OutgoingTrustAcademies()
+        {
+            var outgoingTrustId = Guid.Parse(HttpContext.Session.GetString("OutgoingTrustId"));
+            var result = await _academiesRepository.GetAcademiesByTrustId(outgoingTrustId);
+            var academies = result.Result.Select(a => _getAcademiesMapper.Map(a)).ToList();
+
+            var model = new OutgoingTrustAcademies {Academies = academies};
             return View(model);
         }
     }
