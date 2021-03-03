cd9cf41a5e9473cf9d32b0fe0f9dca8781779bd5
Daniel Burnley <dan@danops.uk>
2021-03-03 12:17:42 +0000

    Temporary login methods

--- Frontend/Controllers/HomeController.cs
+++ Frontend/Controllers/HomeController.cs
@@ -1,23 +1,40 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Frontend.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
 
 namespace Frontend.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
+        [Authorize]
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult Login(string returnUrl)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
             return View();
         }
 
@@ -25,6 +42,39 @@ namespace Frontend.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+
+        public async Task<IActionResult> SubmitLogin(string username, string password, string returnUrl)
+        {
+            var decodedUrl = "";
+
+            if (username != _configuration["username"] || password != _configuration["password"])
+            {
+                TempData["Error.Message"] = "Incorrect username and password";
+                return RedirectToAction("Login", new {returnUrl});
+            }
+
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Name")
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var authenticationProperties = new AuthenticationProperties();
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity), authenticationProperties);
+
+            if (!string.IsNullOrEmpty(returnUrl))
+            {
+                decodedUrl = System.Net.WebUtility.UrlDecode(returnUrl);
+            }
+
+            if (Url.IsLocalUrl(decodedUrl))
+            {
+                return Redirect(returnUrl);
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
