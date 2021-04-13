388dbc1f201d410127e5d00a6848ac97574e2700
Daniel Burnley <dan@danops.uk>
2021-04-13 16:53:46 +0100

    Add document generator with ability to add tables

---
+++ DocumentGeneration.Tests/DocumentTableBuilderTests.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Xunit;
+
+namespace DocumentGeneration.Tests
+{
+    public class DocumentTableBuilderTests
+    {
+        [Fact]
+        public void GivenSingleTableCell_AppendsATableWithASingleCell()
+        {
+            var body = new Body();
+            var builder = new DocumentTableBuilder(body);
+            builder.AddTableRow(new List<string> {"Meow"});
+            builder.Build();
+
+            var createdTableCells = body.Descendants<TableCell>().ToList();
+            var cellContents = createdTableCells.Select(cell => cell.InnerText).ToList();
+
+            Assert.Single(createdTableCells);
+            Assert.Equal("Meow", cellContents[0]);
+        }
+
+        [Fact]
+        public void GivenMultipleCellsInOneRow_AppendsATableWithAMultiCellRow()
+        {
+            var body = new Body();
+            var builder = new DocumentTableBuilder(body);
+            var cellData = new List<string> {"Meow", "Woof", "Quack", "Moo"};
+
+            builder.AddTableRow(cellData);
+            builder.Build();
+
+            var createdTableCells = body.Descendants<TableCell>().ToList();
+            var cellContents = createdTableCells.Select(cell => cell.InnerText).ToList();
+
+            Assert.Equal(4, createdTableCells.Count);
+            Assert.Equal(cellData, cellContents);
+        }
+
+        [Fact]
+        public void GivenMultipleRows_AppendsASingleTableWithAMultipleRows()
+        {
+            var body = new Body();
+            var builder = new DocumentTableBuilder(body);
+            var rowOneData = new List<string> {"Meow", "Woof", "Quack", "Moo"};
+            var rowTwoData = new List<string> {"Cluck", "Howl", "Purr", "Tweet"};
+
+            builder.AddTableRow(rowOneData);
+            builder.AddTableRow(rowTwoData);
+            builder.Build();
+
+            var createdTables = body.Descendants<Table>().ToList();
+            var createdTableRows = createdTables.First().Descendants<TableRow>().ToList();
+            var rowContents = createdTableRows
+                .Select(row =>
+                    row.Descendants<TableCell>().Select(cell => cell.InnerText).ToList()
+                ).ToList();
+
+            Assert.Single(createdTables);
+            Assert.Equal(2, createdTableRows.Count);
+            Assert.Equal(rowOneData, rowContents[0]);
+            Assert.Equal(rowTwoData, rowContents[1]);
+        }
+    }
+}
---
+++ DocumentGeneration/DocumentBuilder.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace DocumentGeneration
+{
+    public class DocumentBuilder
+    {
+        private readonly MemoryStream _memoryStream;
+        private readonly WordprocessingDocument _document;
+        private readonly Body _body;
+
+        public DocumentBuilder(MemoryStream memoryStream)
+        {
+            _memoryStream = memoryStream;
+            _document = WordprocessingDocument.Create(memoryStream, WordprocessingDocumentType.Document);
+            _document.AddMainDocumentPart();
+            _document.MainDocumentPart.Document = new Document(new Body());
+            _body = _document.MainDocumentPart.Document.Body;
+        }
+
+        public void AddParagraph(string text)
+        {
+            var paragraph = new Paragraph();
+            var run = new Run() {RunProperties = new RunProperties()};
+            var runProperties = run.RunProperties;
+            var runText = new Text(text);
+
+            run.AppendChild(runText);
+            paragraph.AppendChild(run);
+            _body.AppendChild(paragraph);
+        }
+
+        public void AddTable(List<List<string>> tableData)
+        {
+            var tableBuilder = new DocumentTableBuilder(_body);
+            tableData.ForEach(row => tableBuilder.AddTableRow(row));
+            tableBuilder.Build();
+        }
+
+        public void Build()
+        {
+            _document.Save();
+            _document.Close();
+        }
+    }
+}
