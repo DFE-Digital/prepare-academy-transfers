aa51db5160f7d82888b6f34940190b08b1944d84
Daniel Burnley <dan@danops.uk>
2021-04-21 11:48:15 +0100

    Get academy from dynamics API for UKPRN

--- Frontend.Tests/ControllerTests/Projects/AcademyPerformanceControllerTests.cs
+++ Frontend.Tests/ControllerTests/Projects/AcademyPerformanceControllerTests.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
 using API.Models.Upstream.Response;
 using API.Repositories;
 using API.Repositories.Interfaces;
+using Data;
+using Data.Models;
 using Frontend.Controllers.Projects;
 using Frontend.Models.AcademyPerformance;
 using Microsoft.AspNetCore.Mvc;
@@ -14,34 +17,89 @@ namespace Frontend.Tests.ControllerTests.Projects
     {
         private readonly AcademyPerformanceController _subject;
         private readonly Mock<IProjectsRepository> _projectRepository;
+        private readonly Mock<IAcademies> _academiesRepository;
+        private readonly Mock<IAcademiesRepository> _dynamicsAcademiesRepository;
 
         public AcademyPerformanceControllerTests()
         {
             _projectRepository = new Mock<IProjectsRepository>();
-            _subject = new AcademyPerformanceController(_projectRepository.Object);
+            _academiesRepository = new Mock<IAcademies>();
+            _dynamicsAcademiesRepository = new Mock<IAcademiesRepository>();
+            _subject = new AcademyPerformanceController(_projectRepository.Object, _academiesRepository.Object,
+                _dynamicsAcademiesRepository.Object);
         }
+
         public class IndexTests : AcademyPerformanceControllerTests
         {
+            private readonly Guid _projectId;
+            private readonly GetProjectsResponseModel _foundProject;
+            private readonly Academy _foundAcademy;
+
+            public IndexTests()
+            {
+                _projectId = Guid.NewGuid();
+                var academyId = Guid.NewGuid();
+
+                _foundProject = new GetProjectsResponseModel
+                {
+                    ProjectId = _projectId,
+                    ProjectAcademies = new List<GetProjectsAcademyResponseModel>
+                        {new GetProjectsAcademyResponseModel {AcademyId = academyId}}
+                };
+
+                var foundDynamicsAcademy = new GetAcademiesModel
+                {
+                    Ukprn = "FoundUKPRN"
+                };
+                
+                _foundAcademy = new Academy
+                {
+                    Ukprn = "ukprn"
+                };
+
+
+                _projectRepository.Setup(r => r.GetProjectById(_projectId)).ReturnsAsync(
+                    new RepositoryResult<GetProjectsResponseModel>
+                    {
+                        Result = _foundProject
+                    });
+
+                _dynamicsAcademiesRepository.Setup(r => r.GetAcademyById(academyId)).ReturnsAsync(
+                    new RepositoryResult<GetAcademiesModel>
+                    {
+                        Result = foundDynamicsAcademy
+                    });
+                
+                _academiesRepository.Setup(r => r.GetAcademyByUkprn("FoundUKPRN")).ReturnsAsync(_foundAcademy);
+            }
+
             [Fact]
             public async void GivenExistingProject_AssignsTheProjectToTheViewModel()
             {
-                var projectId = Guid.NewGuid();
-                var foundProject = new GetProjectsResponseModel()
-                {
-                    ProjectId = projectId
-                };
-                
-                _projectRepository.Setup(r => r.GetProjectById(projectId)).ReturnsAsync(new RepositoryResult<GetProjectsResponseModel>()
-                {
-                    Result = foundProject
-                });
-
-                var response = await _subject.Index(projectId);
+                var response = await _subject.Index(_projectId);
 
                 var viewResponse = Assert.IsType<ViewResult>(response);
                 var viewModel = Assert.IsType<IndexViewModel>(viewResponse.Model);
-                
-                Assert.Equal(foundProject, viewModel.Project);
+
+                Assert.Equal(_foundProject, viewModel.Project);
+            }
+
+            [Fact]
+            public async void GivenDynamicsAcademy_LooksUpTheAcademyWithTheUkprn()
+            {
+                await _subject.Index(_projectId);
+                _academiesRepository.Verify(r => r.GetAcademyByUkprn("FoundUKPRN"), Times.Once);
+            }
+            
+            [Fact]
+            public async void GivenAcademy_AssignsTheProjectToTheViewModel()
+            {
+                var response = await _subject.Index(_projectId);
+
+                var viewResponse = Assert.IsType<ViewResult>(response);
+                var viewModel = Assert.IsType<IndexViewModel>(viewResponse.Model);
+
+                Assert.Equal(_foundAcademy, viewModel.OutgoingAcademy);
             }
         }
     }
--- Frontend/Controllers/Projects/AcademyPerformanceController.cs
+++ Frontend/Controllers/Projects/AcademyPerformanceController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using API.Models.Upstream.Response;
+using API.Repositories;
 using API.Repositories.Interfaces;
 using Data;
 using Data.Models;
@@ -16,17 +17,23 @@ namespace Frontend.Controllers.Projects
     {
         private readonly IProjectsRepository _projectRepository;
         private readonly IAcademies _academiesRepository;
+        private readonly IAcademiesRepository _dynamicsAcademiesRepository;
 
-        public AcademyPerformanceController(IProjectsRepository projectRepository, IAcademies academiesRepository)
+        public AcademyPerformanceController(IProjectsRepository projectRepository, IAcademies academiesRepository,
+            IAcademiesRepository dynamicsAcademiesRepository)
         {
             _projectRepository = projectRepository;
             _academiesRepository = academiesRepository;
+            _dynamicsAcademiesRepository = dynamicsAcademiesRepository;
         }
 
         public async Task<IActionResult> Index(Guid id)
         {
             var getProjectResult = await _projectRepository.GetProjectById(id);
-            var outgoingAcademy = await _academiesRepository.GetAcademyByUkprn("ukprn");
+            var outgoingDynamicsAcademy =
+                await _dynamicsAcademiesRepository.GetAcademyById(getProjectResult.Result.ProjectAcademies[0]
+                    .AcademyId);
+            var outgoingAcademy = await _academiesRepository.GetAcademyByUkprn(outgoingDynamicsAcademy.Result.Ukprn);
 
             var model = new IndexViewModel
             {
