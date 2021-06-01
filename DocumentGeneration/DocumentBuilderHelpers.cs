9f948459a1d55d15e327f42b9ed2627e2878fda3
Daniel Burnley <dan@danops.uk>
2021-06-01 16:54:34 +0100

    Handle null values in document generation

--- DocumentGeneration/DocumentBuilderHelpers.cs
+++ DocumentGeneration/DocumentBuilderHelpers.cs
@@ -9,13 +9,20 @@ namespace DocumentGeneration
     {
         public static void AddTextToElement(OpenXmlElement element, string text)
         {
-            var splitText = text.Split(new[] {"\r\n", "\n", "\r"}, StringSplitOptions.RemoveEmptyEntries);
-            foreach (var line in splitText)
+            if (string.IsNullOrEmpty(text))
             {
-                element.AppendChild(new Text(line));
-                if (line != splitText.Last())
+                element.AppendChild(new Text(""));
+            }
+            else
+            {
+                var splitText = text.Split(new[] {"\r\n", "\n", "\r"}, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var line in splitText)
                 {
-                    element.AppendChild(new Break());
+                    element.AppendChild(new Text(line));
+                    if (line != splitText.Last())
+                    {
+                        element.AppendChild(new Break());
+                    }
                 }
             }
         }
