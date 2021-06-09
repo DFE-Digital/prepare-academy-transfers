381a838627e708107f6bc923f16b4dc3b7847cb1
Daniel Burnley <dan@danops.uk>
2021-06-09 16:25:14 +0100

    Set content type header for API requests

--- Data.TRAMS/TramsHttpClient.cs
+++ Data.TRAMS/TramsHttpClient.cs
@@ -10,6 +10,7 @@ namespace Data.TRAMS
         {
             BaseAddress = new Uri(url);
             DefaultRequestHeaders.Add("ApiKey", apiKey);
+            DefaultRequestHeaders.Add("Content-Type", "application/json");
         }
 
         public new async Task<HttpResponseMessage> GetAsync(string url)
