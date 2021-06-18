ac462061699f1546f5a7034f46289409dde8a162
Daniel Burnley <dan@danops.uk>
2021-06-18 12:46:22 +0100

    Use new builder in create htb document
    Co-authored-by: Stewart Barker <stew.barker@outlook.com>

--- Frontend/Services/CreateHtbDocument.cs
+++ Frontend/Services/CreateHtbDocument.cs
@@ -49,9 +49,125 @@ namespace Frontend.Services
                 builder.AddHeading(hBuilder =>
                 {
                     hBuilder.SetHeadingLevel(HeadingLevel.One);
-                    hBuilder.AddText(projectUrn);
+                    hBuilder.AddText("Headteacher board (HTB) template for:");
                 });
-                builder.AddParagraph(pBuilder => pBuilder.AddText(academy.Name));
+
+                builder.AddHeading(hBuilder =>
+                {
+                    hBuilder.SetHeadingLevel(HeadingLevel.Two);
+                    hBuilder.AddText($"{academy.Name} - URN {academy.Urn}");
+                });
+
+                builder.AddHeading(hBuilder =>
+                {
+                    hBuilder.SetHeadingLevel(HeadingLevel.Two);
+                    hBuilder.AddText($"Outgoing trust - SomeID");
+                });
+
+                builder.AddTable(tBuilder =>
+                {
+                    tBuilder.AddRow(rBuilder =>
+                    {
+                        rBuilder.AddCells(new[]
+                        {
+                            new TextElement {Value = "Recommendation", Bold = true},
+                            new TextElement {Value = "Project recommendation"}
+                        });
+                    });
+
+                    tBuilder.AddRow(rBuilder =>
+                    {
+                        rBuilder.AddCells(new[]
+                        {
+                            new TextElement {Value = "Is an academy order (AO) required?", Bold = true},
+                            new TextElement {Value = "N/A"}
+                        });
+                    });
+
+                    tBuilder.AddRow(rBuilder =>
+                    {
+                        rBuilder.AddCells(new[]
+                        {
+                            new TextElement {Value = "Academy type and route", Bold = true},
+                            new TextElement {Value = academy.EstablishmentType}
+                        });
+                    });
+                });
+
+                builder.AddParagraph(pBuilder => pBuilder.AddText(""));
+
+                builder.AddTable(tBuilder =>
+                {
+                    tBuilder.AddRow(rBuilder =>
+                    {
+                        rBuilder.AddCells(new[]
+                        {
+                            new TextElement {Value = "Date of HTB", Bold = true},
+                            new TextElement {Value = "Date"}
+                        });
+                    });
+
+                    tBuilder.AddRow(rBuilder =>
+                    {
+                        rBuilder.AddCells(new[]
+                        {
+                            new TextElement {Value = "Proposed academy opening date", Bold = true},
+                            new TextElement {Value = "N/A"}
+                        });
+                    });
+
+                    tBuilder.AddRow(rBuilder =>
+                    {
+                        rBuilder.AddCells(new[]
+                        {
+                            new TextElement {Value = "Previous HTB date", Bold = true},
+                            new TextElement {Value = "adwad"}
+                        });
+                    });
+                });
+
+                builder.AddHeading(hBuilder =>
+                {
+                    hBuilder.SetHeadingLevel(HeadingLevel.One);
+                    hBuilder.AddText("KS2 Perf tables");
+                });
+
+                builder.AddTable(tBuilder =>
+                {
+                    tBuilder.AddRow(rBuilder =>
+                    {
+                        rBuilder.AddCells(new[]
+                        {
+                            new TextElement {Value = "", Bold = true},
+                            new TextElement
+                            {
+                                Value = "Percentage meeting expecting standard in reading, writing and maths",
+                                Bold = true
+                            },
+                            new TextElement
+                            {
+                                Value = "Percentage achieving a higher standard in reading, writing and maths",
+                                Bold = true
+                            },
+                            new TextElement {Value = "Reading progress scores", Bold = true},
+                            new TextElement {Value = "Writing progress scores", Bold = true},
+                            new TextElement {Value = "Maths progress scores", Bold = true}
+                        });
+                    });
+
+                    tBuilder.AddRow(rBuilder =>
+                    {
+                        rBuilder.AddCell(new TextElement {Value = academy.Name, Bold = true});
+                        rBuilder.AddCells(new[]
+                        {
+                            "47\n(disadvantaged 20)",
+                            "31\n(disadvantaged 14)",
+                            "Suppressed",
+                            "Suppressed",
+                            "Suppressed"
+                        });
+                    });
+                });
 
                 builder.Build();
             }
