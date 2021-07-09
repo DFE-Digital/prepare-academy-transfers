273139f4ab2c9cc166c2db4ee8ae11f21ecb66d2
StewartBarker <stew.barker@outlook.com>
2021-07-09 12:06:50 +0100

    Remove key stage 2 link from task list if no data exists

--- Frontend/Controllers/ProjectController.cs
+++ Frontend/Controllers/ProjectController.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Data;
+using Data.Models.KeyStagePerformance;
 using Frontend.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -11,10 +14,12 @@ namespace Frontend.Controllers
     public class ProjectController : Controller
     {
         private readonly IProjects _projectRepository;
+        private readonly IEducationPerformance _projectRepositoryEducationPerformance;
 
-        public ProjectController(IProjects projectRepository)
+        public ProjectController(IProjects projectRepository, IEducationPerformance projectRepositoryEducationPerformance)
         {
             _projectRepository = projectRepository;
+            _projectRepositoryEducationPerformance = projectRepositoryEducationPerformance;
         }
         
         public async Task<IActionResult> Index([FromRoute] string id)
@@ -25,6 +30,18 @@ namespace Frontend.Controllers
                 return View("ErrorPage", project.Error.ErrorMessage);
             }
 
+            var educationPerformance =
+                await _projectRepositoryEducationPerformance.GetByAcademyUrn(project.Result.OutgoingAcademyUrn);
+            if (educationPerformance.IsValid)
+            {
+                ViewData["HasKeyStage2PerformanceData"] =
+                    HasKeyStage2PerformanceData(educationPerformance.Result?.KeyStage2Performance);
+            }
+            else
+            {
+                ViewData["HasKeyStage2PerformanceData"] = false;
+            }
+
             var viewModel = new ProjectTaskListViewModel
             {
                 Project = project.Result
@@ -33,6 +50,21 @@ namespace Frontend.Controllers
             ViewData["ProjectId"] = id;
             
             return View(viewModel);
+        }
+
+        private static bool HasKeyStage2PerformanceData(List<KeyStage2> keyStage2Performance)
+        {
+            return keyStage2Performance != null && keyStage2Performance.Any(result => HasValue(result.MathsProgressScore)
+                || HasValue(result.ReadingProgressScore)
+                || HasValue(result.WritingProgressScore)
+                || HasValue(result.PercentageAchievingHigherStdInRWM)
+                || HasValue(result.PercentageMeetingExpectedStdInRWM));
+        }
+
+        private static bool HasValue(DisadvantagedPupilsResult disadvantagedPupilResult)
+        {
+            return !string.IsNullOrEmpty(disadvantagedPupilResult.Disadvantaged) ||
+                   !string.IsNullOrEmpty(disadvantagedPupilResult.NotDisadvantaged);
         }
     }
 }
