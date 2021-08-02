b8595de95cfbd428b58f6ea48b0010128dcac5d4
Daniel Burnley <dan@danops.uk>
2021-08-02 16:51:42 +0100

    Remove unused variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,27 +1,4 @@
 ﻿## ========================================================================== ##
-#  Terraform State S3 Bucket                                                   #
-## ========================================================================== ##
-# variable aws_region {
-#   type        = string
-#   description = "Default region for root module"
-# }
-
-# variable aws_bucket_name {
-#   type        = string
-#   description = "S3 Bucket name"
-# }
-
-# variable aws_bucket_key {
-#   type			= string
-#   description 	= "The Terraform state is written to the key"
-# }
-
-# variable aws_bucket_state_encrypt {
-#   type			= bool
-#   description 	= "Enable server side encryption of the state file"
-# }
-
-## ========================================================================== ##
 #  PaaS						                                                   #
 ## ========================================================================== ##
 variable cf_api_url {
