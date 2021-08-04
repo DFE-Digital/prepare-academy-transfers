89d34e88240f7e078bb75df428efe45b677a893d
StewartBarker <stew.barker@outlook.com>
2021-08-04 16:04:10 +0100

    Remove project and academy repositories as using getinformationforschools service

--- Frontend.Tests/ServicesTests/CreateHtbDocumentTests.cs
+++ Frontend.Tests/ServicesTests/CreateHtbDocumentTests.cs
@@ -14,131 +14,18 @@ namespace Frontend.Tests.ServicesTests
     public class CreateHtbDocumentTests
     {
         private readonly CreateHtbDocument _subject;
-        private readonly Mock<IProjects> _projectsRepository;
-        private readonly Mock<IAcademies> _academiesRepository;
         private readonly Mock<IGetInformationForProject> _getInformationForProject;
         private readonly string _projectUrn = "projectId";
         private readonly string _academyUkprn = "academyId";
-        private readonly Project _foundProject;
-        private readonly Academy _foundAcademy;
 
         public CreateHtbDocumentTests()
         {
-            _projectsRepository = new Mock<IProjects>();
-            _academiesRepository = new Mock<IAcademies>();
             _getInformationForProject = new Mock<IGetInformationForProject>();
-
-            _subject = new CreateHtbDocument(_projectsRepository.Object, _academiesRepository.Object, _getInformationForProject.Object);
-
-            _foundProject = new Project
-            {
-                Urn = _projectUrn,
-                TransferringAcademies = new List<TransferringAcademies>
-                {
-                    new TransferringAcademies {OutgoingAcademyUkprn = _academyUkprn}
-                }
-            };
-
-            _foundAcademy = new Academy
-            {
-                Ukprn = _academyUkprn
-            };
-
-            _projectsRepository.Setup(r => r.GetByUrn(_projectUrn)).ReturnsAsync(
-                new RepositoryResult<Project>
-                {
-                    Result = _foundProject
-                });
-
-            _academiesRepository.Setup(r => r.GetAcademyByUkprn(_academyUkprn)).ReturnsAsync(
-                new RepositoryResult<Academy>
-                {
-                    Result = _foundAcademy
-                });
+            _subject = new CreateHtbDocument(_getInformationForProject.Object);
         }
 
         public class ExecuteTests : CreateHtbDocumentTests
         {
-            [Fact]
-            public async void GivenProjectRepositoryReturnsNotFound_ReturnsCorrectError()
-            {
-                _projectsRepository.Setup(r => r.GetByUrn(It.IsAny<string>())).ReturnsAsync(
-                    new RepositoryResult<Project>
-                    {
-                        Error = new RepositoryResultBase.RepositoryError
-                        {
-                            StatusCode = HttpStatusCode.NotFound,
-                            ErrorMessage = "Example error message"
-                        }
-                    });
-
-                var result = await _subject.Execute(_projectUrn);
-
-                Assert.False(result.IsValid);
-                Assert.Equal(ErrorCode.NotFound, result.ResponseError.ErrorCode);
-                Assert.Equal("Not found", result.ResponseError.ErrorMessage);
-            }
-
-            [Fact]
-            public async void GivenProjectRepositoryReturnsServiceError_ReturnsCorrectError()
-            {
-                _projectsRepository.Setup(r => r.GetByUrn(It.IsAny<string>())).ReturnsAsync(
-                    new RepositoryResult<Project>
-                    {
-                        Error = new RepositoryResultBase.RepositoryError
-                        {
-                            StatusCode = HttpStatusCode.InternalServerError,
-                            ErrorMessage = "Example error message"
-                        }
-                    });
-
-                var result = await _subject.Execute(_projectUrn);
-
-                Assert.False(result.IsValid);
-                Assert.Equal(ErrorCode.ApiError, result.ResponseError.ErrorCode);
-                Assert.Equal("API has encountered an error", result.ResponseError.ErrorMessage);
-            }
-
-            [Fact]
-            public async void GivenAcademyRepositoryReturnsNotFound_ReturnsCorrectError()
-            {
-                _academiesRepository.Setup(r => r.GetAcademyByUkprn(It.IsAny<string>())).ReturnsAsync(
-                    new RepositoryResult<Academy>
-                    {
-                        Error = new RepositoryResultBase.RepositoryError
-                        {
-                            StatusCode = HttpStatusCode.NotFound,
-                            ErrorMessage = "Example error message"
-                        }
-                    });
-
-                var result = await _subject.Execute(_projectUrn);
-
-                Assert.False(result.IsValid);
-                Assert.Equal(ErrorCode.NotFound, result.ResponseError.ErrorCode);
-                Assert.Equal("Not found", result.ResponseError.ErrorMessage);
-            }
-
-            [Fact]
-            public async void GivenAcademyRepositoryReturnsServiceError_ReturnsCorrectError()
-            {
-                _academiesRepository.Setup(r => r.GetAcademyByUkprn(It.IsAny<string>())).ReturnsAsync(
-                    new RepositoryResult<Academy>
-                    {
-                        Error = new RepositoryResultBase.RepositoryError
-                        {
-                            StatusCode = HttpStatusCode.InternalServerError,
-                            ErrorMessage = "Example error message"
-                        }
-                    });
-
-                var result = await _subject.Execute(_projectUrn);
-
-                Assert.False(result.IsValid);
-                Assert.Equal(ErrorCode.ApiError, result.ResponseError.ErrorCode);
-                Assert.Equal("API has encountered an error", result.ResponseError.ErrorMessage);
-            }
-            
             [Fact]
             public async void GivenGetInformationForProjectReturnsNotFound_ReturnsCorrectError()
             {
--- Frontend/Services/CreateHtbDocument.cs
+++ Frontend/Services/CreateHtbDocument.cs
@@ -5,7 +5,6 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Data;
-using Data.Models;
 using Data.Models.KeyStagePerformance;
 using Data.Models.Projects;
 using DocumentGeneration;
@@ -21,14 +20,10 @@ namespace Frontend.Services
 {
     public class CreateHtbDocument : ICreateHtbDocument
     {
-        private readonly IProjects _projectsRepository;
-        private readonly IAcademies _academiesRepository;
         private readonly IGetInformationForProject _getInformationForProject;
         
-        public CreateHtbDocument(IProjects projectsRepository, IAcademies academiesRepository, IGetInformationForProject getInformationForProject )
+        public CreateHtbDocument(IGetInformationForProject getInformationForProject )
         {
-            _projectsRepository = projectsRepository;
-            _academiesRepository = academiesRepository;
             _getInformationForProject = getInformationForProject;
         }
 
@@ -45,19 +40,6 @@ namespace Frontend.Services
 
         public async Task<CreateHtbDocumentResponse> Execute(string projectUrn)
         {
-            var projectResult = await _projectsRepository.GetByUrn(projectUrn);
-            if (!projectResult.IsValid)
-            {
-                return CreateErrorResponse(projectResult.Error);
-            }
-
-            var projectAcademy = projectResult.Result.TransferringAcademies.First();
-            var academyResult = await _academiesRepository.GetAcademyByUkprn(projectAcademy.OutgoingAcademyUkprn);
-            if (!academyResult.IsValid)
-            {
-                return CreateErrorResponse(academyResult.Error);
-            }
-
             var informationForProjectResult = await _getInformationForProject.Execute(projectUrn);
             if (!informationForProjectResult.IsValid)
             {
@@ -430,7 +412,7 @@ namespace Frontend.Services
                 builder.AddHeading(hBuilder =>
                 {
                     hBuilder.SetHeadingLevel(HeadingLevel.Two);
-                    hBuilder.AddText("Attainment 8 Ebacc");
+                    hBuilder.AddText("Attainment 8 EBacc");
                 });
                 
                 builder.AddTable(new[]
@@ -685,7 +667,7 @@ namespace Frontend.Services
                 builder.AddHeading(hBuilder =>
                 {
                     hBuilder.SetHeadingLevel(HeadingLevel.Two);
-                    hBuilder.AddText("Percentage of pupils entering Ebacc");
+                    hBuilder.AddText("Percentage of pupils entering EBacc");
                 });
                 
                 builder.AddTable(new[]
@@ -862,312 +844,7 @@ namespace Frontend.Services
 
             return sb.ToString();
         }
-
-        private static void Title(DocumentBuilder builder, Academy academy)
-        {
-            builder.AddHeading(hBuilder =>
-            {
-                hBuilder.SetHeadingLevel(HeadingLevel.One);
-                hBuilder.AddText("Headteacher board (HTB) template for:");
-            });
-
-            builder.AddHeading(hBuilder =>
-            {
-                hBuilder.SetHeadingLevel(HeadingLevel.Two);
-                hBuilder.AddText($"{academy.Name} - URN {academy.Urn}");
-            });
-
-            builder.AddHeading(hBuilder =>
-            {
-                hBuilder.SetHeadingLevel(HeadingLevel.Two);
-                hBuilder.AddText($"Outgoing trust - SomeID");
-            });
-        }
-
-        private static void HeaderAndFooter(DocumentBuilder builder)
-        {
-            builder.AddHeader(hBuilder =>
-            {
-                hBuilder.AddParagraph(pBuilder =>
-                {
-                    pBuilder.Justify(ParagraphJustification.Center);
-                    pBuilder.AddText(new TextElement {Bold = true, Value = "OFFICIAL"});
-                });
-            });
-
-            builder.AddFooter(fBuilder =>
-            {
-                fBuilder.AddTable(tBuilder =>
-                {
-                    tBuilder.SetBorderStyle(TableBorderStyle.None);
-                    tBuilder.AddRow(rBuilder =>
-                    {
-                        rBuilder.AddCells(new[]
-                        {
-                            "Author: Meow Meowington", "Cleared by: Woofs Barkington", "Version: 01/01/2021"
-                        });
-                    });
-                });
-            });
-        }
-
-        private static void KeyStagePerformanceInformation(DocumentBuilder builder, Academy academy)
-        {
-            builder.AddHeading(hBuilder =>
-            {
-                hBuilder.SetHeadingLevel(HeadingLevel.One);
-                hBuilder.AddText("EXAMPLE Key stage performance tables (KS2)");
-            });
-
-            builder.AddHeading(hBuilder =>
-            {
-                hBuilder.SetHeadingLevel(HeadingLevel.Two);
-                hBuilder.AddText("2019 Key stage 2");
-            });
-
-            builder.AddTable(new[]
-            {
-                new[]
-                {
-                    new TextElement {Value = "", Bold = true},
-                    new TextElement
-                    {
-                        Value = "Percentage meeting expecting standard in reading, writing and maths",
-                        Bold = true
-                    },
-                    new TextElement
-                    {
-                        Value = "Percentage achieving a higher standard in reading, writing and maths",
-                        Bold = true
-                    },
-                    new TextElement {Value = "Reading progress scores", Bold = true},
-                    new TextElement {Value = "Writing progress scores", Bold = true},
-                    new TextElement {Value = "Maths progress scores", Bold = true}
-                },
-
-                new[]
-                {
-                    new TextElement {Value = academy.Name, Bold = true},
-                    new TextElement {Value = "47\n(disadvantaged 20)"},
-                    new TextElement {Value = "31\n(disadvantaged 14)"},
-                    new TextElement {Value = "Suppressed"},
-                    new TextElement {Value = "Suppressed"},
-                    new TextElement {Value = "Suppressed"}
-                },
-                new[]
-                {
-                    new TextElement {Value = "Local authority average", Bold = true},
-                    new TextElement {Value = "63"},
-                    new TextElement {Value = "9"},
-                    new TextElement {Value = "0.2\n(disadvantaged -0.1)"},
-                    new TextElement {Value = "-0.8\n(disadvantaged 0.2)"},
-                    new TextElement {Value = "-0.3\n(disadvantaged 0.2)"}
-                },
-                new[]
-                {
-                    new TextElement {Value = "National average", Bold = true},
-                    new TextElement {Value = "65\n(disadvantaged 51)"},
-                    new TextElement {Value = "11\n(disadvantaged 5)"},
-                    new TextElement {Value = "0.00"},
-                    new TextElement {Value = "0.00"},
-                    new TextElement {Value = "0.00"}
-                }
-            });
-        }
-
-        private static void Rationale(DocumentBuilder builder, Project project)
-        {
-            builder.AddHeading(hBuilder =>
-            {
-                hBuilder.SetHeadingLevel(HeadingLevel.One);
-                hBuilder.AddText("Rationale");
-            });
-
-            builder.AddHeading(hBuilder =>
-            {
-                hBuilder.SetHeadingLevel(HeadingLevel.Two);
-                hBuilder.AddText("Rationale for the project");
-            });
-
-            builder.AddParagraph(pBuilder => pBuilder.AddText(project.Rationale.Project));
-
-            builder.AddHeading(hBuilder =>
-            {
-                hBuilder.SetHeadingLevel(HeadingLevel.One);
-                hBuilder.AddText("Rationale for the trust or sponsor");
-            });
-
-            builder.AddParagraph(pBuilder => pBuilder.AddText(project.Rationale.Trust));
-        }
-
-        private static void GeneralInformation(DocumentBuilder builder, Academy academy)
-        {
-            builder.AddHeading(hBuilder =>
-            {
-                hBuilder.SetHeadingLevel(HeadingLevel.One);
-                hBuilder.AddText("General information");
-            });
-
-            builder.AddTable(new[]
-            {
-                new[]
-                {
-                    new TextElement {Value = "School type", Bold = true},
-                    new TextElement {Value = academy.EstablishmentType}
-                },
-                new[]
-                {
-                    new TextElement {Value = "School phase", Bold = true},
-                    new TextElement {Value = academy.GeneralInformation.SchoolPhase}
-                },
-                new[]
-                {
-                    new TextElement {Value = "Age range", Bold = true},
-                    new TextElement {Value = academy.GeneralInformation.AgeRange}
-                }
-            });
-
-            builder.AddParagraph(pBuilder => pBuilder.AddText(""));
-
-            builder.AddTable(new[]
-            {
-                new[]
-                {
-                    new TextElement {Value = "Capacity", Bold = true},
-                    new TextElement {Value = academy.GeneralInformation.Capacity}
-                },
-                new[]
-                {
-                    new TextElement {Value = "Published admission number (PAN)", Bold = true},
-                    new TextElement {Value = academy.GeneralInformation.Pan}
-                },
-                new[]
-                {
-                    new TextElement {Value = "Percentage the school is full", Bold = true},
-                    new TextElement {Value = academy.GeneralInformation.PercentageFull}
-                },
-                new[]
-                {
-                    new TextElement {Value = "Percentage of free school meals at the school (%FSM)", Bold = true},
-                    new TextElement {Value = ""}
-                }
-            });
-
-            builder.AddParagraph(pBuilder => pBuilder.AddText(""));
-
-            builder.AddTable(new[]
-            {
-                new[]
-                {
-                    new TextElement {Value = "Viability issues", Bold = true},
-                    new TextElement {Value = ""}
-                },
-                new[]
-                {
-                    new TextElement {Value = "Financial surplus or deficit", Bold = true},
-                    new TextElement {Value = ""}
-                },
-                new[]
-                {
-                    new TextElement {Value = "Private finance initiative (PFI) scheme", Bold = true},
-                    new TextElement {Value = ""}
-                }
-            });
-
-            builder.AddParagraph(pBuilder => pBuilder.AddText(""));
-
-            builder.AddTable(new[]
-            {
-                new[]
-                {
-                    new TextElement {Value = "Is this a diocesan multi-academy trust (MAT)", Bold = true},
-                    new TextElement {Value = ""}
-                },
-                new[]
-                {
-                    new TextElement
-                    {
-                        Value = "Percentage of good or outstanding schools in the diocesan trust", Bold = true
-                    },
-                    new TextElement {Value = ""}
-                },
-                new[]
-                {
-                    new TextElement {Value = "Distance from the school to the trust headquarters", Bold = true},
-                    new TextElement {Value = ""}
-                },
-                new[]
-                {
-                    new TextElement {Value = "MP details", Bold = true},
-                    new TextElement {Value = ""}
-                }
-            });
-        }
-
-        private static void IntroductorySection(DocumentBuilder builder, Academy academy, Project project)
-        {
-            builder.AddTable(new[]
-            {
-                new[]
-                {
-                    new TextElement {Value = "Recommendation", Bold = true},
-                    new TextElement {Value = "Project recommendation"}
-                },
-                new[]
-                {
-                    new TextElement {Value = "Academy type and route", Bold = true},
-                    new TextElement {Value = academy.EstablishmentType}
-                },
-                new[]
-                {
-                    new TextElement {Value = "Academy type and route", Bold = true},
-                    new TextElement {Value = academy.EstablishmentType}
-                }
-            });
-
-            builder.AddParagraph(pBuilder => pBuilder.AddText(""));
-
-            builder.AddTable(new[]
-            {
-                new[]
-                {
-                    new TextElement {Value = "Date of HTB", Bold = true},
-                    new TextElement {Value = "Date"}
-                },
-                new[]
-                {
-                    new TextElement {Value = "Proposed academy opening date", Bold = true},
-                    new TextElement {Value = "N/A"}
-                },
-                new[]
-                {
-                    new TextElement {Value = "Previous HTB date", Bold = true},
-                    new TextElement {Value = "adwad"}
-                }
-            });
-
-            builder.AddParagraph(pBuilder => pBuilder.AddText(""));
-
-            builder.AddTable(new[]
-            {
-                new[]
-                {
-                    new TextElement {Value = "Local authority", Bold = true},
-                    new TextElement {Value = "LA"}
-                },
-                new[]
-                {
-                    new TextElement {Value = "Sponsor name", Bold = true},
-                    new TextElement {Value = project.TransferringAcademies[0].IncomingTrustName}
-                },
-                new[]
-                {
-                    new TextElement {Value = "Sponsor reference number", Bold = true},
-                    new TextElement {Value = "SomeNumber"}
-                }
-            });
-        }
-
+        
         private static CreateHtbDocumentResponse CreateErrorResponse(
             RepositoryResultBase.RepositoryError repositoryError)
         {
