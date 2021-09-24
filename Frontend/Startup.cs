7e76739798a51999125c4c7dafd24f140db98522
StewartBarker <stew.barker@outlook.com>
2021-09-24 15:03:03 +0100

    Add secure HTTP headers

--- Frontend/Startup.cs
+++ Frontend/Startup.cs
@@ -134,6 +134,14 @@ namespace Frontend
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.Use(async (context, next) =>
+                {
+                    //context.Response.Headers.Add("X-Frame-Options", "deny");
+                    context.Response.Headers.Add("X-XSS-Protection", "0");
+                    context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+                    context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'");
+                    await next();
+                });
             }
 
             if (!string.IsNullOrEmpty(Configuration["CI"]))
