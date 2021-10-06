c1c5268d4150cc9a8e8c9fb04496502915cd0700
StewartBarker <stew.barker@outlook.com>
2021-10-06 07:29:34 +0100

    Add fields to check if dates are required

--- Data.TRAMS/Models/AcademyTransferProject/AcademyTransferProjectDates.cs
+++ Data.TRAMS/Models/AcademyTransferProject/AcademyTransferProjectDates.cs
@@ -3,7 +3,10 @@ namespace Data.TRAMS.Models.AcademyTransferProject
     public class AcademyTransferProjectDates
     {
         public string HtbDate { get; set; }
+        public bool HasHtbDate { get; set; } = true;
         public string TargetDateForTransfer { get; set; }
+        public bool HasTargetDateForTransfer { get; set; } = true;
         public string TransferFirstDiscussed { get; set; }
+        public bool HasTransferFirstDiscussedDate { get; set; } = true;
     }
 }
