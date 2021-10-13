0cdb6035e9d9c2bc1b50f46759bf4af8aa7210bc
StewartBarker <stew.barker@outlook.com>
2021-10-13 15:31:46 +0100

    wip: disable most security headers to troubleshoot failing cypress tests

--- Frontend/Security/SecureHeadersDefinitions.cs
+++ Frontend/Security/SecureHeadersDefinitions.cs
@@ -9,54 +9,54 @@ namespace Frontend.Security
             public static HeaderPolicyCollection GetHeaderPolicyCollection(bool isDev)
             {
                 var policy = new HeaderPolicyCollection()
-                    .AddFrameOptionsDeny()
-                    .AddXssProtectionBlock()
-                    .AddContentTypeOptionsNoSniff()
-                    .AddReferrerPolicyStrictOriginWhenCrossOrigin()
-                    .RemoveServerHeader()
-                    .AddCrossOriginOpenerPolicy(builder =>
-                    {
-                        builder.SameOrigin();
-                    })
-                    .AddCrossOriginEmbedderPolicy(builder =>
-                    {
-                        builder.RequireCorp();
-                    })
-                    .AddCrossOriginResourcePolicy(builder =>
-                    {
-                        builder.SameOrigin();
-                    })
-                    .AddContentSecurityPolicy(builder =>
-                    {
-                        builder.AddObjectSrc().None();
-                        builder.AddBlockAllMixedContent();
-                        builder.AddImgSrc().Self().From("data:");
-                        builder.AddFormAction().Self();
-                        builder.AddFontSrc().Self();
-                        builder.AddStyleSrc().Self();
-                        builder.AddBaseUri().Self();
-                        builder.AddScriptSrc().From("https://www.googletagmanager.com").UnsafeInline().WithNonce();
-                        builder.AddFrameAncestors().None();
-                    })
-                    .RemoveServerHeader()
-                    .AddPermissionsPolicy(builder =>
-                    {
-                        builder.AddAccelerometer().None();
-                        builder.AddAutoplay().None();
-                        builder.AddCamera().None();
-                        builder.AddEncryptedMedia().None();
-                        builder.AddFullscreen().All();
-                        builder.AddGeolocation().None();
-                        builder.AddGyroscope().None();
-                        builder.AddMagnetometer().None();
-                        builder.AddMicrophone().None();
-                        builder.AddMidi().None();
-                        builder.AddPayment().None();
-                        builder.AddPictureInPicture().None();
-                        builder.AddSyncXHR().None();
-                        builder.AddUsb().None();
-                    });
-
+                    .AddFrameOptionsDeny();
+                    //.AddXssProtectionBlock()
+                    // .AddContentTypeOptionsNoSniff()
+                    // .AddReferrerPolicyStrictOriginWhenCrossOrigin()
+                    // .RemoveServerHeader()
+                    // .AddCrossOriginOpenerPolicy(builder =>
+                    // {
+                    //     builder.SameOrigin();
+                    // })
+                    // .AddCrossOriginEmbedderPolicy(builder =>
+                    // {
+                    //     builder.RequireCorp();
+                    // })
+                    // .AddCrossOriginResourcePolicy(builder =>
+                    // {
+                    //     builder.SameOrigin();
+                    // })
+                    // .AddContentSecurityPolicy(builder =>
+                    // {
+                    //     builder.AddObjectSrc().None();
+                    //     builder.AddBlockAllMixedContent();
+                    //     builder.AddImgSrc().Self().From("data:");
+                    //     builder.AddFormAction().Self();
+                    //     builder.AddFontSrc().Self();
+                    //     builder.AddStyleSrc().Self();
+                    //     builder.AddBaseUri().Self();
+                    //     builder.AddScriptSrc().From("https://www.googletagmanager.com").UnsafeInline().WithNonce();
+                    //     builder.AddFrameAncestors().None();
+                    // })
+                    // .RemoveServerHeader()
+                    // .AddPermissionsPolicy(builder =>
+                    // {
+                    //     builder.AddAccelerometer().None();
+                    //     builder.AddAutoplay().None();
+                    //     builder.AddCamera().None();
+                    //     builder.AddEncryptedMedia().None();
+                    //     builder.AddFullscreen().All();
+                    //     builder.AddGeolocation().None();
+                    //     builder.AddGyroscope().None();
+                    //     builder.AddMagnetometer().None();
+                    //     builder.AddMicrophone().None();
+                    //     builder.AddMidi().None();
+                    //     builder.AddPayment().None();
+                    //     builder.AddPictureInPicture().None();
+                    //     builder.AddSyncXHR().None();
+                    //     builder.AddUsb().None();
+                    // });
+                    
                 if (!isDev)
                 {
                     // max age = one year in seconds
