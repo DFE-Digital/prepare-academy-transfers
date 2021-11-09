6a5ef25481aa14daf370f5ee7a90819b62346d47
Anthony Carbutt <anthony.carbutt@education.gov.uk>
2021-11-09 13:30:47 +0000

    Trust search result

---
+++ Frontend/Validators/Transfers/TrustSearchResultValidator.cs
@@ -0,0 +1,17 @@
+﻿using Data.Models;
+using FluentValidation;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Frontend.Validators.Transfers
+{
+    public class TrustSearchResultValidator : AbstractValidator<List<TrustSearchResult>>
+    {
+        public TrustSearchResultValidator()
+        {
+            RuleFor(x => x)
+                .Must(x => x.Count > 0 && x.Any(t => t.Academies.Count > 0))
+                .WithMessage("We could not find any trusts matching your search criteria");
+        }
+    }
+}
