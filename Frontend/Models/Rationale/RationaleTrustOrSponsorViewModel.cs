49786d8dd7ee69b3ccc0e4b52d3bcac2fcb061a7
StewartBarker <stew.barker@outlook.com>
2021-11-09 15:52:29 +0000

    Add rational trust or sponsor view model

---
+++ Frontend/Models/Rationale/RationaleTrustOrSponsorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Frontend.Models.Rationale
+{
+    public class RationaleTrustOrSponsorViewModel : RationaleCommonViewModel
+    {
+        public string TrustOrSponsorRationale { get; set; }
+    }
+}
