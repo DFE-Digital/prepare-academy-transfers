aa122196f8627d21eb3367ab47d1a7a0e8f4d236
StewartBarker <stew.barker@outlook.com>
2021-11-16 07:20:24 +0000

    Add common view model

---
+++ Frontend/Models/Forms/CommonViewModel.cs
@@ -0,0 +1,9 @@
+namespace Frontend.Models.TransferDates
+{
+    public class CommonViewModel
+    {
+        public string Urn { get; set; }
+        public bool ReturnToPreview { get; set; }
+        public string OutgoingAcademyName { get; set; }
+    }
+}
