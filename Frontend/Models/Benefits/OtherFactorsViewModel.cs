a9ca92208a55be4d7b9596447e47b4c1512133bb
Anthony Carbutt <anthony.carbutt@education.gov.uk>
2021-11-18 16:33:03 +0000

    move classes to separate files

--- Frontend/Models/Benefits/OtherFactorsViewModel.cs
+++ Frontend/Models/Benefits/OtherFactorsViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Data.Models;
-using Data.Models.Projects;
 using Frontend.Models.Forms;
 using Helpers;
 
@@ -10,13 +9,5 @@ namespace Frontend.Models.Benefits
     public class OtherFactorsViewModel : CommonViewModel
     {
         public List<OtherFactorsItemViewModel> OtherFactorsVm { get; set; }
-    }
-
-
-    public class OtherFactorsItemViewModel
-    {
-        public TransferBenefits.OtherFactor OtherFactor { get; set; }
-        public bool Checked { get; set; }
-        public string Description { get; set; }
     }
 }
--- Frontend/Validators/Benefits/OtherFactorsValidator.cs
+++ Frontend/Validators/Benefits/OtherFactorsValidator.cs
@@ -13,15 +13,4 @@ namespace Frontend.Validators.Benefits
         }
       
     }
-
-    public class OtherFactorsItemValidator : AbstractValidator<OtherFactorsItemViewModel>
-    {
-        public OtherFactorsItemValidator()
-        {
-            RuleFor(i => i.Description)
-                .NotEmpty()
-                .When(i => i.Checked)
-                .WithMessage("Specify the concern further");
-        }
-    }
 }
