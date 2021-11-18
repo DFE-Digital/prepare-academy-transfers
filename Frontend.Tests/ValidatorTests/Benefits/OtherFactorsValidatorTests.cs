2a2e61e283a42f6b37f763076569daf33fa24dd9
Anthony Carbutt <anthony.carbutt@education.gov.uk>
2021-11-18 13:08:06 +0000

    Add Unit tests for Other Factors

---
+++ Frontend.Tests/ValidatorTests/Benefits/OtherFactorsValidatorTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Data.Models.Projects;
+using FluentValidation;
+using FluentValidation.TestHelper;
+using Frontend.Models.Benefits;
+using Frontend.Validators.Benefits;
+using Xunit;
+
+namespace Frontend.Tests.ValidatorTests.Benefits
+{
+    public class OtherFactorsValidatorTests : AbstractValidator<OtherFactorsValidator>
+    {
+        private readonly OtherFactorsValidator _otherFactorsValidator;
+        public OtherFactorsValidatorTests() => _otherFactorsValidator = new OtherFactorsValidator();
+        
+        [Fact]
+        public void ShouldHaveChildValidator()
+        {
+            _otherFactorsValidator.ShouldHaveChildValidator(a => a.OtherFactorsVm, typeof(OtherFactorsItemValidator));
+        }
+        
+        [Fact]
+        public async void GivenUrnAndNoOtherFactors_Valid()
+        {
+            var vm = new OtherFactorsViewModel()
+            {
+                ProjectUrn = "0001",
+            };
+
+            var result = await _otherFactorsValidator.TestValidateAsync(vm);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
