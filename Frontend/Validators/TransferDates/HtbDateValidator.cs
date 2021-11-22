e3f6094120fb13e90831371540246480080de64e
StewartBarker <stew.barker@outlook.com>
2021-11-22 08:31:32 +0000

    Add Ab and target date validation

--- Frontend/Validators/TransferDates/HtbDateValidator.cs
+++ Frontend/Validators/TransferDates/HtbDateValidator.cs
@@ -1,5 +1,6 @@
 using FluentValidation;
 using Frontend.Models.TransferDates;
+using Helpers;
 
 namespace Frontend.Validators.TransferDates
 {
@@ -9,6 +10,21 @@ namespace Frontend.Validators.TransferDates
         {
             RuleFor(x => x.HtbDate)
                 .SetValidator(new DateValidator());
+
+            RuleFor(x => x.HtbDate.Date.Day)
+                .Custom((day, context) =>
+                {
+                    if (!context.RootContextData.TryGetValue("TargetDate", out var targetDate)) return;
+                    
+                    var dateVm = context.InstanceToValidate;
+                    if (DatesHelper.SourceDateStringIsGreaterThanToTargetDateString(
+                        dateVm.HtbDate.DateInputAsString(),
+                        targetDate as string) == true)
+                    {
+                        context.AddFailure("The Advisory Board date must be on or before the target date for the transfer");
+                    }
+                });
+
         }
     }
 }
