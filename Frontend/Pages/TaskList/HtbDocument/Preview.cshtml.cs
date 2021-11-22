3229963f0bdc2c102dd1abdd06928b6913d0d7c0
Anthony Carbutt <anthony.carbutt@education.gov.uk>
2021-11-22 15:41:14 +0000

    - Add Features Summary Shared Page - Update preview and features index to use the new shared page

--- Frontend/Pages/TaskList/HtbDocument/Preview.cshtml.cs
+++ Frontend/Pages/TaskList/HtbDocument/Preview.cshtml.cs
@@ -1,15 +1,19 @@
 using System.Threading.Tasks;
 using Frontend.Models;
+using Frontend.Models.Features;
 using Frontend.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Frontend.Pages.TaskList.HtbDocument
 {
+    //todo: remove models and add view models
     public class Preview : ProjectPageModel
     {
         private readonly IGetInformationForProject _getInformationForProject;
         public string ProjectUrn => Project.Urn;
         public object OutgoingAcademyUrn => TransferringAcademy.Urn;
+        
+        public FeaturesViewModel FeaturesViewModel { get; set; }
 
         public Preview(IGetInformationForProject getInformationForProject)
         {
@@ -23,6 +27,17 @@ namespace Frontend.Pages.TaskList.HtbDocument
             TransferringAcademy = response.OutgoingAcademy;
             EducationPerformance = response.EducationPerformance;
 
+            FeaturesViewModel = new FeaturesViewModel
+            {
+                Urn = Project.Urn,
+                IsSubjectToRddOrEsfaIntervention = Project.Features.ReasonForTransfer.IsSubjectToRddOrEsfaIntervention,
+                TypeOfTransfer = Project.Features.TypeOfTransfer,
+                OtherTypeOfTransfer = Project.Features.OtherTypeOfTransfer,
+                OutgoingAcademyUrn = Project.OutgoingAcademyUrn,
+                WhoInitiatedTheTransfer = Project.Features.WhoInitiatedTheTransfer,
+                InterventionDetails = Project.Features.ReasonForTransfer.InterventionDetails,
+                ReturnToPreview = true
+            };
             return Page();
         }
     }
