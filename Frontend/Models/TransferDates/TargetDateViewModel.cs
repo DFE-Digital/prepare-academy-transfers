0621158dd6bb292e5a42a0248b985c1099cfe623
StewartBarker <stew.barker@outlook.com>
2021-11-22 16:21:55 +0000

    Add target date viewmodel

---
+++ Frontend/Models/TransferDates/TargetDateViewModel.cs
@@ -0,0 +1,9 @@
+using Frontend.Models.Forms;
+
+namespace Frontend.Models.TransferDates
+{
+    public class TargetDateViewModel : CommonViewModel
+    {
+        public DateViewModel TargetDate { get; set; }
+    }
+}
