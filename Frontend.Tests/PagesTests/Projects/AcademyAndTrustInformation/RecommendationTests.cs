9f07ad1c25c4496ca10d5ca719bb01f44d900d61
Anthony Carbutt <anthony.carbutt@education.gov.uk>
2021-12-16 14:38:29 +0000

    Post method for Recommendation page and Update to tests

---
+++ Frontend.Tests/PagesTests/Projects/AcademyAndTrustInformation/RecommendationTests.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections.Generic;
+using Data;
+using Data.Models;
+using Data.Models.Projects;
+using Frontend.Models;
+using Frontend.Models.AcademyAndTrustInformation;
+using Frontend.Pages.Projects.AcademyAndTrustInformation;
+using Frontend.Tests.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Routing;
+using Moq;
+using Xunit;
+
+namespace Frontend.Tests.PagesTests.Projects.AcademyAndTrustInformation
+{
+    public class RecommendationTests : PageTests
+    {
+        protected readonly Recommendation Subject;
+
+        protected RecommendationTests()
+        {
+            Subject = new Recommendation(ProjectRepository.Object);
+        }
+    }
+    
+    public class RecommendationGetTests : RecommendationTests
+    {
+        [Fact]
+        public async void GivenUrn_FetchesProjectFromTheRepository()
+        {
+            await Subject.OnGetAsync(ProjectUrn);
+        
+            ProjectRepository.Verify(r => r.GetByUrn(ProjectUrn), Times.Once);
+        }
+        
+        [Fact]
+        public async void GivenReturnToPreview_AssignItToTheView()
+        {
+            var response = await Subject.OnGetAsync(ProjectUrn, true);
+            
+            Assert.IsType<PageResult>(response);
+            Assert.True(Subject.ReturnToPreview);
+        }
+        
+        [Fact]
+        public async void GivenGetByUrnReturnsError_DisplayErrorPage()
+        {
+            var response =  await Subject.OnGetAsync(ProjectErrorUrn);
+            
+            var viewResult = Assert.IsType<ViewResult>(response);
+            Assert.Equal("ErrorPage", viewResult.ViewName);
+            Assert.Equal("Error", viewResult.Model);
+        }
+    }
+    
+    public class RecommendationPostTests : RecommendationTests
+    {
+        private readonly RecommendationViewModel _vm;
+        public RecommendationPostTests()
+        {
+            _vm = new RecommendationViewModel
+            {
+                Urn = ProjectUrn,
+                Recommendation = TransferAcademyAndTrustInformation.RecommendationResult.Approve,
+                Author = "Author"
+            };
+        }
+        [Fact]
+        public async void GivenUrnAndRecommendationAndAuthor_UpdatesTheProject()
+        {
+            await Subject.OnPostAsync(_vm);
+        
+            ProjectRepository.Verify(r =>
+                r.Update(It.Is<Project>(project =>
+                    project.AcademyAndTrustInformation.Recommendation == _vm.Recommendation &&
+                    project.AcademyAndTrustInformation.Author == _vm.Author)), Times.Once);
+        }
+        
+        [Fact]
+        public async void GivenUrnAndRecommendationAndAuthor_RedirectsBackToTheSummary()
+        {
+            var result = await Subject.OnPostAsync(_vm);
+        
+            var routeValues = new RouteValueDictionary(new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("Urn", ProjectUrn)
+            });
+            ControllerTestHelpers.AssertResultRedirectsToPage(result, $"/Projects/AcademyAndTrustInformation/{nameof(Index)}", routeValues);
+        }
+        
+        [Fact]
+        public async void GivenGetByUrnReturnsError_DisplayErrorPage()
+        {
+            var vmError = new RecommendationViewModel
+            {
+                Urn = ProjectErrorUrn,
+                Recommendation = TransferAcademyAndTrustInformation.RecommendationResult.Decline
+            };
+            var response = await Subject.OnPostAsync(vmError);
+            
+            var viewResult = Assert.IsType<ViewResult>(response);
+            Assert.Equal("ErrorPage", viewResult.ViewName);
+            Assert.Equal("Error", viewResult.Model);
+        }
+        
+        [Fact]
+        public async void GivenUpdateReturnsError_DisplayErrorPage()
+        {
+            ProjectRepository.Setup(s => s.Update(It.IsAny<Project>()))
+                .ReturnsAsync(
+                    new RepositoryResult<Project>
+                    {
+                        Error = new RepositoryResultBase.RepositoryError
+                        {
+                            ErrorMessage = "Update error"
+                        }
+                    });
+
+            var response = await Subject.OnPostAsync(_vm);
+            
+            var viewResult = Assert.IsType<ViewResult>(response);
+            Assert.Equal("ErrorPage", viewResult.ViewName);
+            Assert.Equal("Update error", viewResult.Model);
+        }
+        
+        [Fact]
+        public async void GivenReturnToPreview_ReturnToThePreviewPage()
+        {
+            _vm.ReturnToPreview = true;
+            
+            var response = await Subject.OnPostAsync(_vm);
+            
+            ControllerTestHelpers.AssertResultRedirectsToPage(response,
+                Links.HeadteacherBoard.Preview.PageName, new RouteValueDictionary(new {id = ProjectUrn}));
+        }
+    }
+}
---
+++ Frontend/Pages/Projects/AcademyAndTrustInformation/Recommendation.cshtml.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Data;
+using Data.Models.Projects;
+using Frontend.ExtensionMethods;
+using Frontend.Models;
+using Frontend.Models.AcademyAndTrustInformation;
+using Frontend.Models.Forms;
+using Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Frontend.Pages.Projects.AcademyAndTrustInformation
+{
+    public class Recommendation : CommonPageModel
+    {
+        private readonly IProjects _projectRepository;
+        public string OutgoingAcademyUrn { get; set; }
+        public TransferAcademyAndTrustInformation.RecommendationResult RecommendationResult { get; set; }
+        public string Author { get; set; }
+
+        public static List<RadioButtonViewModel> RecommendedRadioButtons(
+            TransferAcademyAndTrustInformation.RecommendationResult selected)
+        {
+            var values =
+                EnumHelpers<TransferAcademyAndTrustInformation.RecommendationResult>.GetDisplayableValues(
+                    TransferAcademyAndTrustInformation.RecommendationResult
+                        .Empty);
+
+            var result = values.Select(value => new RadioButtonViewModel
+            {
+                Value = value.ToString(),
+                Name = "recommendation",
+                DisplayName = value.ToString(),
+                Checked = selected == value
+            }).ToList();
+
+            return result;
+        }
+
+        public Recommendation(IProjects projectRepository)
+        {
+            _projectRepository = projectRepository;
+        }
+
+        public async Task<IActionResult> OnGetAsync(string urn, bool returnToPreview = false)
+        {
+            var project = await _projectRepository.GetByUrn(urn);
+            if (!project.IsValid)
+            {
+                return this.View("ErrorPage", project.Error.ErrorMessage);
+            }
+
+            var projectResult = project.Result;
+
+            Urn = projectResult.Urn;
+            OutgoingAcademyName = projectResult.OutgoingAcademyName;
+            ReturnToPreview = returnToPreview;
+            Author = projectResult.AcademyAndTrustInformation.Author;
+            RecommendationResult = projectResult.AcademyAndTrustInformation.Recommendation;
+            OutgoingAcademyUrn = projectResult.OutgoingAcademyUrn;
+            
+            return Page();
+        }
+        
+        public async Task<IActionResult> OnPostAsync(RecommendationViewModel vm)
+        {
+            var project = await _projectRepository.GetByUrn(vm.Urn);
+            if (!project.IsValid)
+            {
+                return this.View("ErrorPage", project.Error.ErrorMessage);
+            }
+
+            var projectResult = project.Result;
+            projectResult.AcademyAndTrustInformation.Recommendation = vm.Recommendation;
+            projectResult.AcademyAndTrustInformation.Author = vm.Author;
+
+            var result = await _projectRepository.Update(projectResult);
+            if (!result.IsValid)
+            {
+                return this.View("ErrorPage", result.Error.ErrorMessage);
+            }
+
+            if (vm.ReturnToPreview)
+            {
+                return RedirectToPage(Links.HeadteacherBoard.Preview.PageName, new {id = vm.Urn});
+            }
+
+            return RedirectToPage("/Projects/AcademyAndTrustInformation/Index", new {vm.Urn});
+        }
+    }
+}
