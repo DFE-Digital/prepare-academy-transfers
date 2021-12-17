67677ea3b8680bbe626f750ef0712a7b4ea2a715
StewartBarker <50987431+StewartBarker@users.noreply.github.com>
2021-12-17 13:31:33 +0000

    Add integration test set up (#240)
    * Add integration test project
    
    * Update startup to enable integration tests to run
    
    * Add integration tests setup
    
    * Add basic integration test
    
    * Add rationale integration test
    
    * Update fixtures
    
    * Fix random non running tests

---
+++ Frontend.Integration.Tests/BaseIntegrationTests.MockData.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Data.TRAMS.Models;
+using Frontend.Integration.Tests.Fixtures;
+
+namespace Frontend.Integration.Tests
+{
+    public partial class BaseIntegrationTests
+    {
+        protected IEnumerable<TramsProjectSummary> GetProjects(Action<TramsProjectSummary> postSetup = null)
+        {
+            var projects = AcademiesApiFixtures.Projects();
+            if (postSetup != null)
+            {
+                postSetup(projects.First());
+            }
+            
+            foreach (var project in projects)
+            {
+                _factory.AddGetWithJsonResponse($"/trust/{project.TransferringAcademies[0].IncomingTrustUkprn}",  AcademiesApiFixtures.Trust());
+                _factory.AddGetWithJsonResponse(
+                    $"/establishment/{project.TransferringAcademies[0].OutgoingAcademyUkprn}", AcademiesApiFixtures.Establishment());
+            }
+
+            _factory.AddGetWithJsonResponse("/academyTransferProject", projects);
+            return projects;
+        }
+
+        protected TramsProject GetProject(Action<TramsProject> postSetup = null)
+        {
+            var project = AcademiesApiFixtures.Project();
+            if (postSetup != null)
+            {
+                postSetup(project);
+            }
+
+            _factory.AddGetWithJsonResponse($"/academyTransferProject/{project.ProjectUrn}", project);
+            _factory.AddGetWithJsonResponse($"/trust/{project.TransferringAcademies[0].IncomingTrustUkprn}",  AcademiesApiFixtures.Trust());
+            _factory.AddGetWithJsonResponse($"/trust/{project.OutgoingTrustUkprn}",  AcademiesApiFixtures.Trust());
+            _factory.AddGetWithJsonResponse($"/educationPerformance/{project.TransferringAcademies[0].OutgoingAcademy.Urn}",  AcademiesApiFixtures.EducationPerformance());
+            _factory.AddGetWithJsonResponse(
+                $"/establishment/{project.TransferringAcademies[0].OutgoingAcademyUkprn}", AcademiesApiFixtures.Establishment());
+
+            return project;
+        }
+    }
+}
---
+++ Frontend.Integration.Tests/BaseIntegrationTests.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using AngleSharp.Io;
+using AngleSharp.Io.Network;
+using Data.TRAMS.Models;
+using Frontend.Integration.Tests.Fixtures;
+using Xunit;
+
+namespace Frontend.Integration.Tests
+{
+    public partial class BaseIntegrationTests : IClassFixture<IntegrationTestingWebApplicationFactory>, IDisposable
+    {
+        private readonly IntegrationTestingWebApplicationFactory _factory;
+        private readonly IBrowsingContext _browsingContext;
+
+        protected BaseIntegrationTests(IntegrationTestingWebApplicationFactory factory)
+        {
+            _factory = factory;
+            var httpClient = factory.CreateClient();
+            _browsingContext = CreateBrowsingContext(httpClient);
+        }
+
+        protected async Task<IDocument> OpenUrlAsync(string url)
+        {
+            return await _browsingContext.OpenAsync($"http://localhost{url}");
+        }
+
+        protected async Task<IDocument> NavigateAsync(string linkText, int? index = null)
+        {
+            var anchors = Document.QuerySelectorAll("a");
+            var link = (index == null
+                    ? anchors.Single(a => a.TextContent.Contains(linkText))
+                    : anchors.Where(a => a.TextContent.Contains(linkText)).ElementAt(index.Value))
+                as IHtmlAnchorElement;
+
+            return await link.NavigateAsync();
+        }
+        
+        private IBrowsingContext CreateBrowsingContext(HttpClient httpClient)
+        {
+            var config = AngleSharp.Configuration.Default
+                .WithRequester(new HttpClientRequester(httpClient))
+                .WithDefaultLoader(new LoaderOptions { IsResourceLoadingEnabled = true });
+
+            return BrowsingContext.New(config);
+        }
+
+        public IDocument Document => _browsingContext.Active;
+
+        public void Dispose()
+        {
+            _factory.Reset();
+        }
+    }
+}
