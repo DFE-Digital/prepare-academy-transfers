2184e5c1108c7ac817cc038fd3744445dd840961
StewartBarker <50987431+StewartBarker@users.noreply.github.com>
2022-01-11 10:18:54 +0000

    Move pagination out of view and into cs

--- Frontend/Pages/Home/Index.cshtml.cs
+++ Frontend/Pages/Home/Index.cshtml.cs
@@ -5,30 +5,32 @@ using Data.Models;
 using Frontend.ExtensionMethods;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace Frontend.Pages.Home
 {
     public class Index : PageModel
     {
-        private readonly IConfiguration _configuration;
         private readonly IProjects _projectsRepository;
         private readonly ILogger<Index> _logger;
         public List<ProjectSearchResult> Projects;
-        public int Page { get; set; }
+        public int StartingPage { get; private set; } = 1;
+        public bool HasPreviousPage => CurrentPage > 1;
+        public bool HasNextPage => Projects.Count == 10;
+        public int PreviousPage => CurrentPage - 1;
+        public int NextPage => CurrentPage + 1;
 
-        public Index(IConfiguration configuration, IProjects projectsRepository,
-            ILogger<Index> logger)
+        [BindProperty(SupportsGet = true)] public int CurrentPage { get; set; } = 1;
+
+        public Index(IProjects projectsRepository, ILogger<Index> logger)
         {
-            _configuration = configuration;
             _projectsRepository = projectsRepository;
             _logger = logger;
         }
-        
-        public async Task<IActionResult> OnGet()
+
+        public async Task<IActionResult> OnGetAsync()
         {
-            var projects = await _projectsRepository.GetProjects(Page);
+            var projects = await _projectsRepository.GetProjects(CurrentPage);
             if (!projects.IsValid)
             {
                 return this.View("ErrorPage", projects.Error.ErrorMessage);
@@ -36,6 +38,11 @@ namespace Frontend.Pages.Home
 
             _logger.LogInformation("Home page loaded");
             Projects = projects.Result;
+
+            if (CurrentPage - 5 > 1)
+            {
+                StartingPage = CurrentPage - 5;
+            }
 
             return Page();
         }
