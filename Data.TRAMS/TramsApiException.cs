1ec37e40ef690f97bbb15213683ca16968971deb
StewartBarker <50987431+StewartBarker@users.noreply.github.com>
2022-01-14 08:43:04 +0000

    Add Custom Tram API exception

---
+++ Data.TRAMS/TramsApiException.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Net;
+using System.Net.Http;
+
+namespace Data.TRAMS
+{
+    public class TramsApiException  : Exception
+    {
+        public HttpStatusCode StatusCode { get; }
+
+        public TramsApiException(HttpResponseMessage httpResponseMessage, string message = null) 
+            : base(message ?? $"API encountered an error-({httpResponseMessage.StatusCode})")
+        {
+            StatusCode = httpResponseMessage.StatusCode;
+            base.Data.Add("Sentry:Tag:StatusCode", StatusCode);
+            base.Data.Add("Content", httpResponseMessage.Content.ReadAsStringAsync().Result);
+        }
+    }
+}
