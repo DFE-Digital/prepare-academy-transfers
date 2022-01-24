8ad1e287348ca4204fc4cc60755420607fd20e4b
acarbutt <30597375+acarbutt@users.noreply.github.com>
2022-01-24 09:21:14 +0000

    Techdebt/82307 move pupil numbers to Razor page (#265)

---
+++ Frontend.Tests/PagesTests/Projects/PupilNumbersTests.cs
@@ -0,0 +1,85 @@
+﻿using Data.Models;
+using Frontend.Models;
+using Frontend.Models.Forms;
+using Frontend.Pages.Projects;
+using Frontend.Tests.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Routing;
+using Moq;
+using Xunit;
+
+namespace Frontend.Tests.PagesTests.Projects
+{
+    public class PupilNumbersTests : BaseTests
+    {
+        private readonly PupilNumbers _subject;
+        private const string AdditionalInformation =  "Pupil numbers additional information";
+        
+        public PupilNumbersTests()
+        {
+            _subject = new PupilNumbers(GetInformationForProject.Object, ProjectRepository.Object)
+            {
+                Urn = ProjectUrn0001,
+                AdditionalInformationViewModel = new AdditionalInformationViewModel()
+                {
+                   AdditionalInformation = AdditionalInformation
+                }
+            };
+        }
+
+        public class GetTests : PupilNumbersTests
+        {
+            [Fact]
+            public async void GivenProjectId_GetsInformationAboutProject()
+            {
+                await _subject.OnGetAsync();
+                GetInformationForProject.Verify(s => s.Execute(ProjectUrn0001), Times.Once);
+            }
+
+            [Fact]
+            public async void GivenExistingAdditionalInformation_AssignsTheInformationToTheViewModel()
+            {
+                FoundInformationForProject.Project.PupilNumbersAdditionalInformation =
+                    AdditionalInformation;
+                var response = await _subject.OnGetAsync();
+
+                Assert.Equal(AdditionalInformation,
+                    _subject.AdditionalInformationViewModel.AdditionalInformation);
+            }
+        }
+
+        public class PostTests : PupilNumbersTests
+        {
+            [Fact]
+            public async void GivenAdditionalInformationIsPosted_CorrectlyRedirects()
+            {
+                var response = await _subject.OnPostAsync();
+
+                ControllerTestHelpers.AssertResultRedirectsToPage(response, "/Projects/PupilNumbers",
+                    new RouteValueDictionary(new {Urn = ProjectUrn0001}));
+            }
+
+            [Fact]
+            public async void GivenAdditionalInformation_UpdatesTheProjectCorrectly()
+            {
+                await _subject.OnPostAsync();
+
+                ProjectRepository.Verify(r => r.Update(It.Is<Project>(
+                    project => project.PupilNumbersAdditionalInformation == AdditionalInformation
+                )));
+            }
+
+            [Fact]
+            public async void GivenReturnToPreview_RedirectToPreviewPage()
+            {
+                _subject.ReturnToPreview = true;
+                var response = await _subject.OnPostAsync();
+                ControllerTestHelpers.AssertResultRedirectsToPage(
+                    response, Links.HeadteacherBoard.Preview.PageName,
+                    new RouteValueDictionary(new {Urn = ProjectUrn0001})
+                );
+            }
+        }
+    }
+}
---
+++ Frontend/Pages/Projects/PupilNumbers.cshtml.cs
@@ -0,0 +1,73 @@
+﻿using System.Threading.Tasks;
+using Data;
+using Frontend.Models;
+using Frontend.Models.Forms;
+using Frontend.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Frontend.Pages.Projects
+{
+    public class PupilNumbers : CommonPageModel
+    {
+        protected const string FragmentEdit = "additional-information-hint";
+        public string GirlsOnRoll { get; set; }
+        public string BoysOnRoll { get; set; }
+        public string WithStatementOfSEN { get; set; }
+        public string WithEAL { get; set; }
+        public string FreeSchoolMealsLast6Years { get; set; }
+        
+        [BindProperty]
+        public AdditionalInformationViewModel AdditionalInformationViewModel { get; set; }
+        public bool IsPreview { get; set; }
+        
+        [BindProperty(SupportsGet = true)]
+        public bool AddOrEditAdditionalInformation { get; set; }
+
+        private readonly IGetInformationForProject _getInformationForProject;
+        private readonly IProjects _projectsRepository;
+
+        public PupilNumbers(IGetInformationForProject getInformationForProject, IProjects projectsRepository)
+        {
+            _getInformationForProject = getInformationForProject;
+            _projectsRepository = projectsRepository;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var projectInformation = await _getInformationForProject.Execute(Urn);
+            GirlsOnRoll = projectInformation.OutgoingAcademy.PupilNumbers.GirlsOnRoll;
+            BoysOnRoll = projectInformation.OutgoingAcademy.PupilNumbers.BoysOnRoll;
+            WithStatementOfSEN = projectInformation.OutgoingAcademy.PupilNumbers.WithStatementOfSen;
+            WithEAL = projectInformation.OutgoingAcademy.PupilNumbers.WhoseFirstLanguageIsNotEnglish;
+            FreeSchoolMealsLast6Years = projectInformation.OutgoingAcademy.PupilNumbers
+                .PercentageEligibleForFreeSchoolMealsDuringLast6Years;
+            OutgoingAcademyUrn = projectInformation.OutgoingAcademy.Urn;
+            OutgoingAcademyName = projectInformation.OutgoingAcademy.Name;
+            AdditionalInformationViewModel = new AdditionalInformationViewModel
+            {
+                AdditionalInformation = projectInformation.Project.PupilNumbersAdditionalInformation,
+                HintText =
+                    "If you add comments, they'll be included in the pupil numbers section of your project template.",
+                Urn = projectInformation.Project.Urn,
+                AddOrEditAdditionalInformation = AddOrEditAdditionalInformation,
+                ReturnToPreview = ReturnToPreview
+            };
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var model = await _projectsRepository.GetByUrn(Urn);
+
+            model.Result.PupilNumbersAdditionalInformation = AdditionalInformationViewModel.AdditionalInformation;
+            await _projectsRepository.Update(model.Result);
+
+            if (ReturnToPreview)
+            {
+                return RedirectToPage(Links.HeadteacherBoard.Preview.PageName, new {Urn});
+            }
+
+            return RedirectToPage("/Projects/PupilNumbers", null, new {Urn}, FragmentEdit);
+        }
+    }
+}
