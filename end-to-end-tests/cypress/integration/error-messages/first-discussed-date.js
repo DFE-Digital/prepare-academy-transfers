be1e610b0b136a41797632f477d85ee234b3d4d9
acarbutt <30597375+acarbutt@users.noreply.github.com>
2022-01-24 15:36:04 +0000

    Update first-discussed-date.js

--- end-to-end-tests/cypress/integration/error-messages/first-discussed-date.js
+++ end-to-end-tests/cypress/integration/error-messages/first-discussed-date.js
@@ -27,7 +27,7 @@
         cy.clickDataTest("first-discussed");
         cy.fillInDate(Cypress.dayjs().add(10,'M'))
         cy.get('.govuk-button').click();
-        cy.get('.govuk-error-summary__body > .govuk-list > li > a').should('have.text', 'You must enter a past date').should('be.visible');
-        cy.get('#FirstDiscussed\\.Date\\.Day-error').should('have.text', 'Error:You must enter a past date').should('be.visible');
+        cy.get('.govuk-error-summary__body > .govuk-list > li > a').should('have.text', 'You must type a date in the past').should('be.visible');
+        cy.get('#FirstDiscussed\\.Date\\.Day-error').should('have.text', 'Error:You must type a date in the past').should('be.visible');
     });
 });
