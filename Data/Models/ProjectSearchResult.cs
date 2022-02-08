17cf7c28cfd249db45010ec219d013e7193198ab
StewartBarker <50987431+StewartBarker@users.noreply.github.com>
2022-02-08 11:36:02 +0000

    Add fields to project search result

--- Data/Models/ProjectSearchResult.cs
+++ Data/Models/ProjectSearchResult.cs
@@ -6,14 +6,10 @@ namespace Data.Models
     public class ProjectSearchResult
     {
         public string Name { get; set; }
-        
         public string Reference { get; set; }
+        public string OutgoingTrustUkprn { get; set; }
         public string OutgoingTrustName { get; set; }
         public List<TransferringAcademies> TransferringAcademies { get; set; }
         public string Urn { get; set; }
-        
-        public string IncomingTrustName => TransferringAcademies[0].IncomingTrustName;
-        public string OutgoingAcademyName => TransferringAcademies[0].OutgoingAcademyName;
-        public string OutgoingAcademyUrn => TransferringAcademies[0].OutgoingAcademyUrn;
     }
 }
