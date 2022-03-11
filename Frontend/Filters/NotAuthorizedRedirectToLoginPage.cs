ff467170d844601c86df87d5f5b80e8655798ac4
acarbutt <30597375+acarbutt@users.noreply.github.com>
2022-03-11 10:11:52 +0000

    Feature/85763 session time out page (#308)

---
+++ Frontend/Filters/NotAuthorizedRedirectToLoginPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Frontend.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class NotAuthorizedRedirectToLoginPage : AllowAnonymousAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                context.Result = new RedirectToPageResult("Login");
+            }
+        }
+    }
+}
