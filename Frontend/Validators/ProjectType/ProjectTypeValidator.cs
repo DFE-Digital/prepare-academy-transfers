f5c89bb5e3d1c5237c17736fb898156f393a735f
MartinWheelerMT <martin.wheeler@madetech.com>
2022-09-13 11:06:39 +0100

    Add Validator for project type page

---
+++ Frontend/Validators/ProjectType/ProjectTypeValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Frontend.Models;
+
+namespace Frontend.Validators.ProjectType
+{
+	public class ProjectTypeValidator : AbstractValidator<ProjectTypeViewModel>
+	{
+		public ProjectTypeValidator()
+		{
+			RuleFor(vm => vm.Type)
+				.NotNull()
+				.WithMessage("Select a project type");
+		}
+	}
+}
