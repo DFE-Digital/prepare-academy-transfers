376baed38f336c278dddf5a9e511b409046ac802
Dominic Need <dominic.need@nimbleapproach.com>
2022-10-03 11:04:13 +0100

    Correct null cases for status validation

--- Frontend/Services/TaskListService.cs
+++ Frontend/Services/TaskListService.cs
@@ -81,8 +81,8 @@ namespace Frontend.Services
         }
         private ProjectStatuses GetLegalRequirementsStatus(Project project)
         {
-            if (project.LegalRequirements.DiocesanConsent == null && project.LegalRequirements.DiocesanConsent == null &&
-                    project.LegalRequirements.DiocesanConsent == null){
+            if (project.LegalRequirements.DiocesanConsent == null && project.LegalRequirements.FoundationConsent == null &&
+                    project.LegalRequirements.TrustAgreement == null){
                 return ProjectStatuses.NotStarted;
             }
             return project.LegalRequirements.IsCompleted == true ? ProjectStatuses.Completed : ProjectStatuses.InProgress;
