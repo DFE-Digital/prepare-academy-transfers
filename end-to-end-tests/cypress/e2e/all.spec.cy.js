5352f402dac1d527c8b3c732fb3dd2a5ad75a3ef
Lisa Sau <lisasau85@gmail.com>
2022-10-11 10:14:47 +0100

    Task 108911: Upgrade cypress 10 & add test tags
    - update to import path

--- end-to-end-tests/cypress/e2e/all.spec.cy.js
+++ end-to-end-tests/cypress/e2e/all.spec.cy.js
@@ -1,28 +1,29 @@
 /// <reference types ='Cypress'/>
+
 // Run all tests NOTE: this is a work around as cypress 10.0 GUI does not allow to run all tests
 
 // Benefits-and-risks
-import '../e2e/benefits-and-risks/equalities-impact-assessment.cy'
+import './benefits-and-risks/equalities-impact-assessment.cy'
 
 // End2EndTests
-import '../e2e/End2EndTests/project-runthrough-ks2.cy'
-import '../e2e/End2EndTests/project-runthrough.cy'
+import './End2EndTests/project-runthrough-ks2.cy'
+import './End2EndTests/project-runthrough.cy'
 
 // Error-messages
-import '../e2e/error-messages/advisory-board-date.cy'
-import '../e2e/error-messages/target-date.cy'
+import './error-messages/advisory-board-date.cy'
+import './error-messages/target-date.cy'
 
 // Landing-page
-import '../e2e/landing-page/landing-page.cy'
+import './landing-page/landing-page.cy'
 
 // Multiple-academies
-import '../e2e/multiple-academies/select-multiple-academies.cy'
+import './multiple-academies/select-multiple-academies.cy'
 
 // Performance
-import '../e2e/Performance/preview and download template.cy'
+import './Performance/preview and download template.cy'
 
 // SQLTests
-import '../e2e/SQLTests/DatabaseTests.cy'
+import './SQLTests/DatabaseTests.cy'
 
 // Trusts-transfers
-import '../e2e/trust-transfers/search-incoming-trust.cy'
+import './trust-transfers/search-incoming-trust.cy'
