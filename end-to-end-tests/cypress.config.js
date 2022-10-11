1fdb3331f8943a430546790bce5fc59db06b5cf3
Lisa Sau <lisasau85@gmail.com>
2022-10-11 10:19:38 +0100

    Task 108911: Upgrade cypress 10 & add test tags
    - removing unused IDs

--- end-to-end-tests/cypress.config.js
+++ end-to-end-tests/cypress.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "jd9h7w",
   video: false,
 
   env: {
@@ -17,8 +16,6 @@ module.exports = defineConfig({
     },
   },
 
-  tenantId: "fad277c9-c60a-4da1-b5f3-b3b8b34a82f9",
-  clientId: "8b708989-014c-451c-a564-b2bed382f61f",
   modifyObstructiveCode: true,
   chromeWebSecurity: false,
 
