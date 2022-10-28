1d2d2c29ba41a2577d2130924abb818d485fd1fe
Jordan Beard <jordan.beard@nimbleapproach.com>
2022-10-28 15:16:55 +0100

    AB#107471 cookies - add cookie banner and page.

---
+++ Frontend/Pages/Shared/_CookieBanner.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Frontend.Pages.Shared
+{
+    public class _CookieBannerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
