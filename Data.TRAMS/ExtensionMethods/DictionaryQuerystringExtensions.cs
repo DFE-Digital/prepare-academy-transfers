9a7765da49934ecdad650b0e45991cfc43af253d
WILDE <Tim.WILDE@EDUCATION.GOV.UK>
2022-11-08 10:33:25 +0000

    Dictionary to Querystring helper.

--- Data.TRAMS/ExtensionMethods/DictionaryQuerystringExtensions.cs
+++ Data.TRAMS/ExtensionMethods/DictionaryQuerystringExtensions.cs
@@ -9,20 +9,32 @@ namespace Data.TRAMS.ExtensionMethods
       /// <summary>
       ///    Converts a <see cref="IDictionary{string, string}" /> to an encoded querystring.
       /// </summary>
-      /// <param name="parameters">An <see cref="IDictionary{string, string}" /> containing parameter names (of <see cref="string"/>) and values (of <see cref="string"/>)</param>
+      /// <param name="parameters">
+      ///    An <see cref="IDictionary{string, string}" /> containing parameter names (of
+      ///    <see cref="string" />) and values (of <see cref="string" />)
+      /// </param>
       /// <param name="prefix">A <see cref="bool" /> defining whether the querystring should have a '?' prefix (default: true)</param>
+      /// <param name="keepEmpty">
+      ///    A <see cref="bool" /> defining whether keys with null/empty values should be kept (default:
+      ///    true)
+      /// </param>
       /// <returns>A string representing the parameters combined, UrlEncoded and (optionally) prefixed ready to be used in a URI</returns>
-      /// <remarks>
-      ///    <paramref name="parameters" /> entries with either a null/empty key or value will be excluded as the framework
-      ///    UrlEncoder will throw for null values.
-      /// </remarks>
-      public static string ToQueryString(this IDictionary<string, string> parameters, bool prefix = true)
+      public static string ToQueryString(this IDictionary<string, string> parameters, bool prefix = true,
+         bool keepEmpty = true)
       {
-         IEnumerable<string> parameterPairs = parameters
-            .Where(x => string.IsNullOrWhiteSpace(x.Key) is false && string.IsNullOrWhiteSpace(x.Value) is false)
-            .Select(x => $"{UrlEncoder.Default.Encode(x.Key)}={UrlEncoder.Default.Encode(x.Value)}");
+         IList<string> parameterPairs = parameters
+            .Where(x => keepEmpty || string.IsNullOrWhiteSpace(x.Value) is false)
+            .Select(x => $"{Encode(x.Key)}={Encode(x.Value)}")
+            .ToList();
 
-         return $"{(prefix ? "?" : string.Empty)}{string.Join("&", parameterPairs)}";
+         return parameterPairs.Count > 0
+            ? $"{(prefix ? "?" : string.Empty)}{string.Join("&", parameterPairs)}"
+            : string.Empty;
+
+         string Encode(string x)
+         {
+            return string.IsNullOrWhiteSpace(x) ? string.Empty : UrlEncoder.Default.Encode(x);
+         }
       }
    }
 }
