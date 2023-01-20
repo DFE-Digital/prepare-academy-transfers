c449cdea5b1f65fa9dd7cc0f168f57a1e9beee65
WILDE <Tim.WILDE@EDUCATION.GOV.UK>
2023-01-20 11:53:00 +0000

    Dynamic back link mechanism.

--- Frontend/Models/Links.cs
+++ Frontend/Models/Links.cs
@@ -1,53 +1,96 @@
-namespace Frontend.Models
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+
+namespace Frontend.Models;
+
+public static class Links
 {
-    public static class Links
-    {
-        public static class HeadteacherBoard
-        {
-            public static readonly LinkItem Preview = new LinkItem
-                {PageName = "/TaskList/HtbDocument/Preview", BackText = "Back to preview page"};
-        }
-        
-        public static class ProjectType
-        {
-            public static readonly LinkItem Index = new LinkItem {BackText = "Back to project type", PageName = "/ProjectType/Index"};
-        }
-        public static class Project
-        {
-            public static readonly LinkItem Index = new LinkItem { BackText = "Back to project", PageName = "/Projects/Index" };
-        }
-        public static class ProjectList
-        {
-            public static readonly LinkItem Index = new LinkItem { PageName = "/Home/Index" };
-        }
-        public static class ProjectAssignment
-        {
-            public static readonly LinkItem Index = new LinkItem {  PageName = "/Projects/ProjectAssignment/Index" };
-        }
-        public static class LegalRequirements
-        {
-            public static readonly LinkItem Index = new LinkItem { PageName = "/Projects/LegalRequirements/Index" };
-                
-            public static class IncomingTrustAgreement
-            {
-                public static readonly LinkItem Index = new LinkItem { PageName = "/Projects/LegalRequirements/IncomingTrustAgreement" };
-            }
-            public static class DiocesanConsent
-            {
-                public static readonly LinkItem Index = new LinkItem { PageName = "/Projects/LegalRequirements/DiocesanConsent" };
-            }
-            public static class OutgoingTrustConsent
-            {
-                public static readonly LinkItem Index = new LinkItem { PageName = "/Projects/LegalRequirements/OutgoingTrustConsent" };
-            }
-        }
+   private static readonly IDictionary<string, LinkItem> LinkCache = new ConcurrentDictionary<string, LinkItem>();
 
-        public static readonly LinkItem CookiePreferences = new LinkItem { PageName = "/CookiePreferences" };        
-    }
+   public static readonly LinkItem CookiePreferences = Create(page: "/CookiePreferences");
 
-    public class LinkItem
-    {
-        public string PageName { get; set; }
-        public string BackText { get; set; }
-    }
+   private static LinkItem Create(string page, string text = "Back")
+   {
+      var item = new LinkItem { PageName = page, BackText = text };
+      LinkCache.Add(page, item);
+      return item;
+   }
+
+   public static LinkItem FindByPageName(string page)
+   {
+      if (string.IsNullOrWhiteSpace(page)) return default;
+
+      return LinkCache.ContainsKey(page) ? LinkCache[page] : default;
+   }
+
+   public static class HeadteacherBoard
+   {
+      public static readonly LinkItem Preview =
+         Create("/TaskList/HtbDocument/Preview", "Back to preview");
+   }
+
+   public static class ProjectType
+   {
+      public static readonly LinkItem Index =
+         Create("/ProjectType/Index", "Back to project type");
+   }
+
+   public static class Project
+   {
+      public static readonly LinkItem Index = Create("/Projects/Index", "Back to project");
+   }
+
+   public static class ProjectList
+   {
+      public static readonly LinkItem Index = Create("/Home/Index");
+   }
+
+   public static class ProjectAssignment
+   {
+      public static readonly LinkItem Index = Create("/Projects/ProjectAssignment/Index");
+   }
+
+   public static class LegalRequirements
+   {
+      public static readonly LinkItem Index = Create("/Projects/LegalRequirements/Index");
+
+      public static class IncomingTrustAgreement
+      {
+         public static readonly LinkItem Index =
+            Create("/Projects/LegalRequirements/IncomingTrustAgreement");
+      }
+
+      public static class DiocesanConsent
+      {
+         public static readonly LinkItem Index = Create("/Projects/LegalRequirements/DiocesanConsent");
+      }
+
+      public static class OutgoingTrustConsent
+      {
+         public static readonly LinkItem Index = Create("/Projects/LegalRequirements/OutgoingTrustConsent");
+      }
+   }
+}
+
+public class LinkItem
+{
+   public string PageName { get; set; }
+   public string BackText { get; set; }
+   public string Urn { get; set; }
+
+   public LinkItem For(string urn)
+   {
+      return new LinkItem { BackText = BackText, PageName = PageName, Urn = urn };
+   }
+
+   public LinkItem OverrideFrom(IQueryCollection query)
+   {
+      return new LinkItem
+      {
+         BackText = query.ContainsKey("bt") ? query["bt"] : BackText,
+         PageName = query.ContainsKey("bl") ? query["bl"] : PageName,
+         Urn = query.ContainsKey("u") ? query["u"] : Urn
+      };
+   }
 }
