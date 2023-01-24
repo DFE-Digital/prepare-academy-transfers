c1a95569a0aed31e1b208f0e862024e781dc7158
Jordan Beard <jordan.beard@nimbleapproach.com>
2023-01-24 10:55:12 +0000

    experimenting with main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,19 +3,15 @@ resource cloudfoundry_app worker_app {
 	space              = data.cloudfoundry_space.space.id
 	docker_image       = local.docker_image
 	strategy           = "blue-green-v3"
-
 	service_binding {
 		service_instance = cloudfoundry_service_instance.redis.id
 	}
-
 	service_binding {
 		service_instance = cloudfoundry_user_provided_service.logit.id
 	}
-
 	routes {
 		route = cloudfoundry_route.web_app_cloudapp_digital_route.id
 	}
-
 	environment = {
 		"ASPNETCORE_ENVIRONMENT" = var.aspnetcore_environment
 		"ASPNETCORE_URLS"        = "http://+:8080"
@@ -28,22 +24,21 @@ resource cloudfoundry_app worker_app {
 		"AzureAd__TenantId"        = var.app_azuread_tenantid
 		"AzureAd__GroupId"         = var.app_azuread_groupid
 		"CypressTestSecret"        = var.app_cypresstest_secret
-		"SupportEmail"             = var.app_support_email      
+		"SupportEmail"             = var.app_support_email
+      "AccessRequest"            = var.app_accessrequest
+      "TemplateFeedback"         = var.app_templatefeedback
 	}
 }
-
 resource cloudfoundry_route web_app_cloudapp_digital_route {
 	domain   = data.cloudfoundry_domain.london_cloud_apps_digital.id
 	space    = data.cloudfoundry_space.space.id
 	hostname = local.web_app_name
 }
-
 resource cloudfoundry_service_instance redis {
 	name         = local.redis_service_name
 	space        = data.cloudfoundry_space.space.id
 	service_plan = data.cloudfoundry_service.redis.service_plans[var.cf_redis_service_plan]
 }
-
 resource cloudfoundry_user_provided_service logit {
 	name             = local.logit_service_name
 	space            = data.cloudfoundry_space.space.id
