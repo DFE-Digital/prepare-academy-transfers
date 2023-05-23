3a2a1e50e2a144eaf3734260278443f96ffac8dc
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2023-05-23 11:09:48 +0100

    Add CDN origin Hostname and Host Header overrides

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -20,6 +20,8 @@ locals {
   cdn_frontdoor_host_add_response_headers      = var.cdn_frontdoor_host_add_response_headers
   cdn_frontdoor_custom_domains                 = var.cdn_frontdoor_custom_domains
   cdn_frontdoor_host_redirects                 = var.cdn_frontdoor_host_redirects
+  cdn_frontdoor_origin_fqdn_override           = var.cdn_frontdoor_origin_fqdn_override
+  cdn_frontdoor_origin_host_header_override    = var.cdn_frontdoor_origin_host_header_override
   key_vault_access_users                       = toset(var.key_vault_access_users)
   key_vault_access_ipv4                        = var.key_vault_access_ipv4
   tfvars_filename                              = var.tfvars_filename
