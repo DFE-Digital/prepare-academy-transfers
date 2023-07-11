2a2bc55c2c0fcb6173d6838ddec8bf26679833b3
Chris Dexter <109349584+chrisdexnimble@users.noreply.github.com>
2023-07-11 12:26:09 +0100

    refactored to remove some excess code. Added a test to make sure correlation id is set by default on requests

---
+++ Dfe.PrepareTransfers.Data.TRAMS.Tests/TramsHttpClientTests.cs
@@ -0,0 +1,38 @@
+﻿using Dfe.Academisation.CorrelationIdMiddleware;
+using Dfe.PrepareTransfers.Data.TRAMS.Tests.Helpers;
+using FluentAssertions;
+using RichardSzalay.MockHttp;
+using Xunit;
+using System;
+using System.Net.Http;
+
+namespace Dfe.PrepareTransfers.Data.TRAMS.Tests
+{
+    public class TramsHttpClientTests
+    {
+        [Fact]
+        public void CorrelationId_Headers_Should_Be_Added_To_Requests()
+        {
+            var factory = new FakeClientFactory();
+
+            var correlationContext = new CorrelationContext();
+            correlationContext.SetContext(Guid.NewGuid());
+
+            var sut = new TramsHttpClient(factory, correlationContext);
+
+            factory.CreatedHttpClient.DefaultRequestHeaders.Should().ContainSingle(x => x.Key == "x-correlationId");
+        }
+
+        private class FakeClientFactory : IHttpClientFactory
+        {
+            public HttpClient CreatedHttpClient { get; private set; }
+
+            public HttpClient CreateClient(string name)
+            {
+                IHttpClientFactory innerFactory = new MockHttpClientFactory(new MockHttpMessageHandler());
+                CreatedHttpClient = innerFactory.CreateClient();
+                return CreatedHttpClient;
+            }
+        }
+    }
+}
--- Dfe.PrepareTransfers.Data.TRAMS/TramsHttpClient.cs
+++ Dfe.PrepareTransfers.Data.TRAMS/TramsHttpClient.cs
@@ -6,32 +6,28 @@ namespace Dfe.PrepareTransfers.Data.TRAMS
 
     public class TramsHttpClient : ITramsHttpClient
     {
-        private readonly ICorrelationContext _correlationContext;
         private readonly HttpClient _httpClient;
 
         public TramsHttpClient(IHttpClientFactory httpClientFactory, ICorrelationContext correlationContext)
         {
             _httpClient = httpClientFactory.CreateClient("TramsApiClient");
-            _correlationContext = correlationContext;
+            _httpClient.DefaultRequestHeaders.Add(Dfe.Academisation.CorrelationIdMiddleware.Keys.HeaderKey, correlationContext.CorrelationId.ToString());
         }
 
         public async Task<HttpResponseMessage> GetAsync(string url)
         {
-            _httpClient.DefaultRequestHeaders.Add(Dfe.Academisation.CorrelationIdMiddleware.Keys.HeaderKey, _correlationContext.CorrelationId.ToString());
             var response = await _httpClient.GetAsync(url);
             return response;
         }
 
         public async Task<HttpResponseMessage> PostAsync(string url, HttpContent content)
         {
-            _httpClient.DefaultRequestHeaders.Add(Dfe.Academisation.CorrelationIdMiddleware.Keys.HeaderKey, _correlationContext.CorrelationId.ToString());
             var response = await _httpClient.PostAsync(url, content);
             return response;
         }
 
         public async Task<HttpResponseMessage> PatchAsync(string url, HttpContent content)
         {
-            _httpClient.DefaultRequestHeaders.Add(Dfe.Academisation.CorrelationIdMiddleware.Keys.HeaderKey, _correlationContext.CorrelationId.ToString());
             var response = await _httpClient.PatchAsync(url, content);
             return response;
         }
