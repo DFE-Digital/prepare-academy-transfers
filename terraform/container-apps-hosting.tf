86285cda9e139adff0fcb19101cb76fa3166fdba
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2023-07-18 13:51:21 +0100

    Deploy an Event Hub consumer
    This allows us to export logs to an external centralised log platform

--- terraform/container-apps-hosting.tf
+++ terraform/container-apps-hosting.tf
@@ -10,7 +10,9 @@ module "azure_container_apps_hosting" {
 
   enable_container_registry = local.enable_container_registry
 
-  enable_event_hub = local.enable_event_hub
+  enable_event_hub                          = local.enable_event_hub
+  enable_logstash_consumer                  = local.enable_logstash_consumer
+  eventhub_export_log_analytics_table_names = local.eventhub_export_log_analytics_table_names
 
   enable_dns_zone      = local.enable_dns_zone
   dns_zone_domain_name = local.dns_zone_domain_name
