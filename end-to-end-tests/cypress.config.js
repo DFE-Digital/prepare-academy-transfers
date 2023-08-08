eb8d2ce3958ba2529b428c09a079014d4753dbf5
Chris Sherlock <chris.sherlock@nimbleapproach.com>
2023-08-08 13:14:07 +0100

    Rework security test workflow

--- end-to-end-tests/cypress.config.js
+++ end-to-end-tests/cypress.config.js
@@ -30,7 +30,7 @@ module.exports = defineConfig({
       })
 
       on('after:run', async () => {
-        if(process.env.zapReport) {
+        if(process.env.zap) {
           await generateZapReport()
         }
       })
