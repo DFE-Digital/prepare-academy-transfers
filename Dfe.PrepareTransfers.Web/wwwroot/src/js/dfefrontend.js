3ccea84e8dfb37bef2165b5f867ada83a2d44464
Dominic NEED <Dominic.NEED@EDUCATION.GOV.UK>
2023-11-17 15:30:38 +0000

    DfE Front end

---
+++ Dfe.PrepareTransfers.Web/wwwroot/src/js/dfefrontend.js
@@ -0,0 +1,11 @@
+
+
+// Components
+import Header from './components/header/header';
+
+import './polyfills';
+
+// Initialize components
+document.addEventListener('DOMContentLoaded', () => {
+  Header();
+});
