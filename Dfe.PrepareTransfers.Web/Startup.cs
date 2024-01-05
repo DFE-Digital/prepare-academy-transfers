0ae0094f7ac1a4693b89b97ce7c69ec6a5742e0d
plockwood <paul.lockwood@education.gov.uk>
2024-01-05 12:04:20 +0000

    added maintenance page as anonymous page

--- Dfe.PrepareTransfers.Web/Startup.cs
+++ Dfe.PrepareTransfers.Web/Startup.cs
@@ -79,6 +79,7 @@ public class Startup
            {
                options.Conventions.AuthorizeFolder("/");
                options.Conventions.AllowAnonymousToPage("/AccessibilityStatement");
+               options.Conventions.AllowAnonymousToPage("/Maintenance");
                options.Conventions.AllowAnonymousToPage("/SessionTimedOut");
            })
            .AddViewOptions(options => { options.HtmlHelperOptions.ClientValidationEnabled = false; });
