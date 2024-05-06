afb83bc3471c57dd54b07d1154f60360f268e378
Your Name <yourname@example.com>
2024-05-06 11:54:58 +0100

    Adjusting logic to match conversions

--- Dfe.PrepareTransfers.Web/Helpers/KeyStage4DataStatusHelper.cs
+++ Dfe.PrepareTransfers.Web/Helpers/KeyStage4DataStatusHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace Dfe.PrepareTransfers.Web.Helpers
@@ -7,7 +8,7 @@ namespace Dfe.PrepareTransfers.Web.Helpers
 	{
 		public static string KeyStageDataTag(DateTime date)
 		{
-			string status = DetermineKeyStageDataStatus(date);
+			string status = DetermineKeyStageDataStatus(date, KeyStages.KS4);
 			var colour = status.ToLower() switch
 			{
 				"revised" => "orange",
@@ -17,27 +18,75 @@ namespace Dfe.PrepareTransfers.Web.Helpers
 			};
 			return $"<td class='govuk-table__cell'><strong class='govuk-tag govuk-tag--{colour}'>{status}</strong></td>";
 		}
+        public enum StatusType
+        {
+            Provisional,
+            Revised,
+            Final
+        }
+        public enum StatusColour
+        {
+            Grey,
+            Orange,
+            Green
+        }
+        public enum KeyStages
+        {
+            KS2,
+            KS4,
+            KS5
+        }
+        
+        private static readonly Dictionary<StatusType, (StatusColour Colour, string Description)> StatusMap = new()
+        {
+            { StatusType.Provisional, (StatusColour.Grey, StatusType.Provisional.ToString()) },
+            { StatusType.Revised, (StatusColour.Orange, StatusType.Revised.ToString()) },
+            { StatusType.Final, (StatusColour.Green, StatusType.Final.ToString()) },
+        };
+        public static string DetermineKeyStageDataStatus(DateTime date, KeyStages keyStage)
+        {
+            
+            bool isItCurrentAcademicYear =
+                (date.Month < 9 && date.Year == DateTime.Now.Year) ||
+                (date.Month >= 9 && date.Year == DateTime.Now.Year - 1);
 
-		public static string DetermineKeyStageDataStatus(DateTime date)
-		{
-			// Check where and which academic year the tag is in relation too
-			bool isItCurrentAcademicYear = date.Month < 9 && date.Year == DateTime.Now.Year ||
-			                               date.Month >= 9 && date.Year == DateTime.Now.Year - 1;
-			var status = isItCurrentAcademicYear switch
-			{
-				// Rules - KS4 – Provisional October, Revised January; Final April
-				true => date.Month switch
-				{
-					>= 9 => "Provisional",
-					<= 4 => "Revised",
-					> 4 => "Final"
-				},
-				false => "Final"
-			};
-			return status;
-		}
+            bool isItLastAcademicYear =
+                (date.Month < 9 && date.Year == DateTime.Now.Year - 1) ||
+                (date.Month >= 9 && date.Year == DateTime.Now.Year - 2);
 
-		public static string KeyStageDataRow()
+            StatusType statusType = StatusType.Final;
+
+            if (isItCurrentAcademicYear)
+            {
+                statusType = StatusType.Provisional;
+            }
+            if (isItLastAcademicYear)
+            {
+                statusType = DetermineStatusType(date, keyStage);
+            }
+
+            return statusType.ToString();
+        }
+        private static StatusType DetermineStatusType(DateTime date, KeyStages keyStage)
+        {
+            return keyStage switch
+            {
+                KeyStages.KS2 => date.Month switch
+                {
+                    >= 9 => StatusType.Provisional,
+                    <= 3 => StatusType.Revised,
+                    _ => StatusType.Final
+                },
+                KeyStages.KS4 or KeyStages.KS5 => date.Month switch
+                {
+                    >= 9 => StatusType.Provisional,
+                    <= 4 => StatusType.Revised,
+                    _ => StatusType.Final
+                },
+                _ => throw new ArgumentException("Invalid key stage")
+            };
+        }
+        public static string KeyStageDataRow()
 		{
 			StringBuilder rowString = new("<tr class='govuk-table__row'>");
 			rowString.Append("<th scope='row' class='govuk-table__header'>Status</th>");
