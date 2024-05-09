2d129445a73f9e6b0201605de1cd056acabb67fe
Elijah Aremu <45821029+elielijah321@users.noreply.github.com>
2024-05-09 11:22:28 +0100

    add page for update incoming trust

---
+++ Dfe.PrepareTransfers.Web/Pages/Projects/AcademyAndTrustInformation/UpdateIncomingTrust.cshtml.cs
@@ -0,0 +1,59 @@
+using Dfe.PrepareTransfers.Data;
+using FluentValidation.AspNetCore;
+using Dfe.PrepareTransfers.Web.Validators.Transfers;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Dfe.PrepareTransfers.Web.Models;
+
+namespace Dfe.PrepareTransfers.Web.Pages.Projects.AcademyAndTrustInformation
+{
+    public class UpdateIncomingTrustModel : CommonPageModel
+    {
+        private readonly IProjects _projectRepository;
+
+        [BindProperty(Name = "query", SupportsGet = true)]
+
+        public string SearchQuery { get; set; }
+
+
+        public UpdateIncomingTrustModel(IProjects projectRepository)
+        {
+            _projectRepository = projectRepository;
+        }
+
+        public async Task<IActionResult> OnGetAsync(string urn, bool returnToPreview = false)
+        {
+            var project = await _projectRepository.GetByUrn(urn);
+
+            var projectResult = project.Result;
+
+            Urn = projectResult.Urn;
+            ReturnToPreview = returnToPreview;
+            IncomingTrustName = projectResult.IncomingTrustName;
+            OutgoingAcademyUrn = projectResult.OutgoingAcademyUrn;
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            //var validator = new EditIncomingTrustNameValidator();
+            //var validationResults = await validator.ValidateAsync(this);
+            //validationResults.AddToModelState(ModelState, null);
+
+            if (!ModelState.IsValid)
+            {
+                return await OnGetAsync(Urn);
+            }
+
+            await _projectRepository.UpdateIncomingTrustName(Urn, IncomingTrustName);
+
+            if (ReturnToPreview)
+            {
+                return RedirectToPage(Links.HeadteacherBoard.Preview.PageName, new { Urn });
+            }
+
+            return RedirectToPage("/Projects/AcademyAndTrustInformation/Index", new { Urn });
+        }
+    }
+}
