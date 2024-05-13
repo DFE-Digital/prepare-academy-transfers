f87961341a37a344b10bf8c2b1ec7701a5cbc398
Elijah Aremu <45821029+elielijah321@users.noreply.github.com>
2024-05-13 12:49:40 +0100

    change the incoming trust

---
+++ Dfe.PrepareTransfers.Web/Models/AutoCompleteSearchModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Dfe.PrepareTransfers.Web.Models;
+
+public class AutoCompleteSearchModel
+{
+   public AutoCompleteSearchModel(string label, string searchQuery, string searchEndpoint)
+   {
+      Label = label;
+      SearchQuery = searchQuery?.Replace("'", "\\'");
+      SearchEndpoint = searchEndpoint;
+   }
+
+   public string Label { get; set; }
+
+   public string SearchQuery { get; set; }
+
+   public string SearchEndpoint { get; set; }
+}
--- Dfe.PrepareTransfers.Web/Pages/Projects/AcademyAndTrustInformation/UpdateIncomingTrust.cshtml.cs
+++ Dfe.PrepareTransfers.Web/Pages/Projects/AcademyAndTrustInformation/UpdateIncomingTrust.cshtml.cs
@@ -1,59 +1,132 @@
 using Dfe.PrepareTransfers.Data;
-using FluentValidation.AspNetCore;
-using Dfe.PrepareTransfers.Web.Validators.Transfers;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Dfe.PrepareTransfers.Web.Models;
+using Dfe.PrepareTransfers.Services;
+using System;
+using Dfe.PrepareTransfers.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Dfe.Academisation.ExtensionMethods;
 
 namespace Dfe.PrepareTransfers.Web.Pages.Projects.AcademyAndTrustInformation
 {
     public class UpdateIncomingTrustModel : CommonPageModel
     {
-        private readonly IProjects _projectRepository;
+        private const string SEARCH_LABEL =
+           "Enter the name, UKPRN (UK Provider Reference Number) or Companies House number.";
 
-        [BindProperty(Name = "query", SupportsGet = true)]
+        private const string SEARCH_ENDPOINT = "/project/0/academy-and-trust-information/update-incoming-trust?handler=Search&searchQuery=";
+        private readonly ErrorService _errorService;
+        private readonly ITrusts _trustsRepository;
+        private readonly IProjects _repository;
 
-        public string SearchQuery { get; set; }
-
-
-        public UpdateIncomingTrustModel(IProjects projectRepository)
+        public UpdateIncomingTrustModel(ITrusts trustsRepository, IProjects projectRepository, ErrorService errorService)
         {
-            _projectRepository = projectRepository;
+            _trustsRepository = trustsRepository;
+            _repository = projectRepository;
+            _errorService = errorService;
+            AutoCompleteSearchModel = new AutoCompleteSearchModel(SEARCH_LABEL, string.Empty, SEARCH_ENDPOINT);
         }
 
-        public async Task<IActionResult> OnGetAsync(string urn, bool returnToPreview = false)
+        [BindProperty]
+        public string SearchQuery { get; set; } = "";
+        public AutoCompleteSearchModel AutoCompleteSearchModel { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int urn)
         {
-            var project = await _projectRepository.GetByUrn(urn);
-
-            var projectResult = project.Result;
-
-            Urn = projectResult.Urn;
-            ReturnToPreview = returnToPreview;
-            IncomingTrustName = projectResult.IncomingTrustName;
-            OutgoingAcademyUrn = projectResult.OutgoingAcademyUrn;
+            AutoCompleteSearchModel = new AutoCompleteSearchModel(SEARCH_LABEL, SearchQuery, SEARCH_ENDPOINT);
 
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnGetSearch(string searchQuery)
         {
-            //var validator = new EditIncomingTrustNameValidator();
-            //var validationResults = await validator.ValidateAsync(this);
-            //validationResults.AddToModelState(ModelState, null);
+            string[] searchSplit = SplitOnBrackets(searchQuery);
 
-            if (!ModelState.IsValid)
+            List<Trust> trusts = await _trustsRepository.SearchTrusts(searchSplit[0].Trim());
+
+            return new JsonResult(trusts.Select(t =>
             {
-                return await OnGetAsync(Urn);
+                string displayUkprn = string.IsNullOrWhiteSpace(t.Ukprn) ? string.Empty : $"({t.Ukprn})";
+                string suggestion = $@"{t.Name?.ToTitleCase() ?? ""} {displayUkprn}
+            <br />
+            Companies House number: {t.CompaniesHouseNumber ?? ""}";
+                return new { suggestion = HighlightSearchMatch(suggestion, searchSplit[0].Trim(), t), value = $"{t.Name?.ToTitleCase() ?? ""} ({t.Ukprn})" };
+            }));
+        }
+
+        public async Task<IActionResult> OnPostAsync(int urn)
+        {
+            AutoCompleteSearchModel = new AutoCompleteSearchModel(SEARCH_LABEL, SearchQuery, SEARCH_ENDPOINT);
+            if (string.IsNullOrWhiteSpace(SearchQuery))
+            {
+                ModelState.AddModelError(nameof(SearchQuery), "Enter the Trust name, UKPRN or Companies House number");
+                _errorService.AddErrors(ModelState.Keys, ModelState);
+                return Page();
             }
 
-            await _projectRepository.UpdateIncomingTrustName(Urn, IncomingTrustName);
-
-            if (ReturnToPreview)
+            string[] searchSplit = SplitOnBrackets(SearchQuery);
+            if (searchSplit.Length < 2)
             {
-                return RedirectToPage(Links.HeadteacherBoard.Preview.PageName, new { Urn });
+                ModelState.AddModelError(nameof(SearchQuery), "We could not find any trusts matching your search criteria");
+                _errorService.AddErrors(ModelState.Keys, ModelState);
+                return Page();
             }
 
-            return RedirectToPage("/Projects/AcademyAndTrustInformation/Index", new { Urn });
+            string ukprn = searchSplit[searchSplit.Length - 1];
+            string trustName = searchSplit[0];
+
+            List<Trust> trusts = await _trustsRepository.SearchTrusts(ukprn);
+
+            if (trusts.Count != 1)
+            {
+                ModelState.AddModelError(nameof(SearchQuery), "We could not find a trust matching your search criteria");
+                _errorService.AddErrors(ModelState.Keys, ModelState);
+                return Page();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var trust = trusts.First();
+
+                    await _repository.UpdateIncomingTrustName(urn.ToString(), trustName, ukprn);
+
+
+                    return RedirectToPage("/Projects/AcademyAndTrustInformation/Index", new { urn });
+
+                }
+                catch (Exception ex)
+                {
+
+                    _errorService.AddApiError();
+                    return Page();
+                }
+
+            }
+            return Page();
+        }
+
+        private static string HighlightSearchMatch(string input, string toReplace, Trust trust)
+        {
+            if (trust == null ||
+                string.IsNullOrWhiteSpace(trust.Name))
+                return string.Empty;
+
+            int index = input.IndexOf(toReplace, StringComparison.InvariantCultureIgnoreCase);
+            string correctCaseSearchString = input.Substring(index, toReplace.Length);
+
+            return input.Replace(toReplace, $"<strong>{correctCaseSearchString}</strong>", StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        private static string[] SplitOnBrackets(string input)
+        {
+            // return array containing one empty string if input string is null or empty
+            if (string.IsNullOrWhiteSpace(input)) return new string[1] { string.Empty };
+
+            return input.Split(new[] { '(', ')' }, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
         }
     }
 }
